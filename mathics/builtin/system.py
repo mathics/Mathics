4727dc81f04dd41512b95f7c239a90bb14218622
Jan Pöschko <jan@poeschko.com>
2012-06-25 02:28:55 +0200

    added Names

--- mathics/builtin/system.py
+++ mathics/builtin/system.py
@@ -4,8 +4,10 @@
 System functions
 """
 
-from mathics.core.expression import String
-from mathics.builtin.base import Predefined
+import re
+
+from mathics.core.expression import Expression, String
+from mathics.builtin.base import Builtin, Predefined
 from mathics import get_version_string
 
 class Version(Predefined):
@@ -23,3 +25,55 @@ class Version(Predefined):
     
     def evaluate(self, evaluation):
         return String(get_version_string(True))
+    
+class Names(Builtin):
+    """
+    <dl>
+    <dt>'Names["$pattern$"]'
+        <dd>returns the list of names matching $pattern$.
+    </dl>
+    
+    >> Names["List"]
+     = {List}
+    >> Names["List*"]
+     = {List, ListQ, Listable}
+    >> Names["List@"]
+     = {Listable}
+     
+    >> x = 5;
+    >> Names["Global`*"]
+     = {x}
+     
+    The number of built-in symbols:
+    >> Length[Names["System`*"]]
+     = ...
+     
+    #> Length[Names["System`*"]] > 350
+     = True
+    """
+    
+    def apply(self, pattern, evaluation):
+        'Names[pattern_String]'
+        
+        pattern = pattern.get_string_value()
+        if pattern is None:
+            return
+        
+        if pattern.startswith('System`'):
+            names = evaluation.definitions.get_builtin_names()
+        elif pattern.startswith('Global`'):
+            names = evaluation.definitions.get_user_names() - evaluation.definitions.get_builtin_names()
+        else:
+            names = evaluation.definitions.get_names()
+        if '`' in pattern:
+            pattern = pattern[pattern.find('`')+1:]
+            
+        pattern = pattern.replace('@', '[a-z]+').replace('*', '.*')
+        pattern = re.compile('^' + pattern + '$')
+        def match_pattern(name):
+            return pattern.match(name) is not None
+            
+        names = [name for name in names if match_pattern(name)]
+        names.sort()
+        
+        return Expression('List', *[String(name) for name in names])
