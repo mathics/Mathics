7816f559d7d64ee66b59e7f4a2ad6dd173b93d24
Angus Griffith <16sn6uv@gmail.com>
2012-07-01 04:55:59 +1000

    Polygons handled by ExtrudeGeometry which performs the triangulation for general polygons, even those with holes or S-shapes.

--- mathics/web/media/js/graphics3d.js
+++ mathics/web/media/js/graphics3d.js
@@ -42,29 +42,80 @@ function drawPlot3D(prim) {
 function drawPolygon(prim) {    
   //TODO: Handle arbitary polygons. For example, S-shapes, shapes with holes.
 
-  var mesh, poly, material;
+  var mesh, polypath, polyshape, material;
 
   // console.log("drawPolygon");
 
-  poly = new THREE.Geometry();
-  var center = new THREE.Vector3(0,0,0);
+  // Find three points
+  var p1 = new THREE.Vector4(prim.coords[0][0][0], prim.coords[0][0][1], prim.coords[0][0][2]);
+  var p2 = new THREE.Vector4(prim.coords[1][0][0], prim.coords[1][0][1], prim.coords[1][0][2]);
+  var p3 = new THREE.Vector4(prim.coords[2][0][0], prim.coords[2][0][1], prim.coords[2][0][2]);
 
+  //TODO: Check the points are not colinear
+
+  // And Three Vectors
+  var v1 = new THREE.Vector3();
+  var v2 = new THREE.Vector3();
+  var v3 = new THREE.Vector3(); // normal vector
+
+  v1.sub(p2, p1);
+  v2.sub(p3, p1);
+  v3.cross(v1, v2);
+
+  var normal = new THREE.Vector4(v3.x, v3.y, v3.z, -v3.dot(p1));  // Point p on the plane iff p.normal = 0
+
+  // Point Closest to origin - translation
+  var nearest = new THREE.Vector3(
+            normal.x*normal.w / (normal.x*normal.x + normal.y*normal.y + normal.z*normal.z),
+            normal.y*normal.w / (normal.x*normal.x + normal.y*normal.y + normal.z*normal.z),
+            normal.z*normal.w / (normal.x*normal.x + normal.y*normal.y + normal.z*normal.z)
+  );
+
+  // Angles to the z axis - rotaton
+  var thetax = Math.acos(normal.z / Math.sqrt(normal.y*normal.y + normal.z*normal.z));
+  var thetay = Math.acos(normal.z / Math.sqrt(normal.x*normal.x + normal.z*normal.z));
+  
+  // Linear Transformation Matrix - Rotation + Translation
+  var L = new THREE.Matrix4();
+  L.makeTranslation(-nearest.x, -nearest.y, -nearest.z);  
+  L.multiplySelf(new THREE.Matrix4().makeRotationX(thetax));
+  L.multiplySelf(new THREE.Matrix4().makeRotationY(thetay));
+
+  polypath = new THREE.Path();
   for (var i = 0; i < prim.coords.length; i++) {
-    var tmpv = prim.coords[i][0];
-    poly.vertices.push(new THREE.Vector3(tmpv[0], tmpv[1], tmpv[2]));
-    center.x += tmpv[0] * (1.0 / prim.coords.length);
-    center.y += tmpv[1] * (1.0 / prim.coords.length);
-    center.z += tmpv[2] * (1.0 / prim.coords.length);
+    tmpv = new THREE.Vector4(prim.coords[i][0][0], prim.coords[i][0][1], prim.coords[i][0][2], 1);
+    L.multiplyVector4(tmpv);
+    if (i == 0){
+        polypath.moveTo(tmpv.x, tmpv.y);
+    } else {
+        polypath.lineTo(tmpv.x, tmpv.y);
+    }
   }
-  poly.vertices.push(center);
+  polypath.lineTo(polypath.curves[0].v1.x, polypath.curves[0].v1.y); // Close the curve
 
-  for (var i = 0; i < prim.coords.length-1; i++) {
-    poly.faces.push(new THREE.Face3(prim.coords.length,i,i+1));
+  polyshape = polypath.toShapes();
+
+  console.log(polyshape);
+
+  polygeom = new THREE.ExtrudeGeometry(polyshape, {amount:0.1});
+
+  for (var i=0; i < polygeom.vertices.length; i++) {
+      polygeom.vertices[i].z = 0.0;
   }
-  poly.faces.push(new THREE.Face3(prim.coords.length,prim.coords.length-1,0));
 
-  mesh = new THREE.Mesh(poly, new THREE.MeshNormalMaterial());
-  mesh.doubleSided = true;
+  // Undo the Linear Transformation
+  var Li = new THREE.Matrix4();
+  Li.getInverse(L);
+
+  for (var i=0; i < polygeom.vertices.length; i++) {
+      tmpv = new THREE.Vector4(polygeom.vertices.x, polygeom.vertices.y, polygeom.vertices.z, 1.0);
+      Li.multiplyVector4(tmpv);
+      polygeom.vertices.x = tmpv.x
+      polygeom.vertices.y = tmpv.y;
+      polygeom.vertices.z = tmpv.z;
+  }
+
+  mesh = new THREE.Mesh(polygeom, new THREE.MeshBasicMaterial({color: 0x000000}));
 
   return mesh;
 }
@@ -169,17 +220,22 @@ function drawGraphics3D(container, data) {
   }
 
   function ScaleInView() {
-    if (camera instanceof THREE.PerspectiveCamera) 
+    if (camera instanceof THREE.OrthographicCamera) {
+      //
+    } else if (camera instanceof THREE.PerspectiveCamera) {
       camera.fov = 0.0;
+    }
 
     for (var i=0; i<8; i++) {
+      //console.log(axes.geometry.vertices[i]);
+
       proj2d = toScreenXY(axes.geometry.vertices[i],camera);
 
       if (camera instanceof THREE.OrthographicCamera) {
         //
       } else if (camera instanceof THREE.PerspectiveCamera) {
         angle = 57.296 * Math.max(Math.atan(proj2d.x/proj2d.z), Math.atan(proj2d.y/proj2d.z));
-        camera.fov = Math.max(camera.fov,0.7*angle);
+        camera.fov = Math.max(camera.fov, 0.5*angle);
       }
     }
     camera.updateProjectionMatrix();
