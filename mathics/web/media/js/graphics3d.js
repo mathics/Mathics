9aa30bd388219da17ea677dc7e40ac70bb834a84
Angus Griffith <16sn6uv@gmail.com>
2012-07-20 03:16:04 +1000

    Try to keep label divs within the canvas dimensions; hide them when zoomed in.

--- mathics/web/media/js/graphics3d.js
+++ mathics/web/media/js/graphics3d.js
@@ -359,6 +359,12 @@ function drawGraphics3D(container, data) {
           var tickpos = toCanvasCoords(ticks[i][j].geometry.vertices[0]);
           ticknums[i][j].style.left = tickpos.x.toString() + "px";
           ticknums[i][j].style.top = tickpos.y.toString() + "px";
+          if (tickpos.x < 5 || tickpos.x > 395 || tickpos.y < 5 || tickpos.y > 395) {
+            ticknums[i][j].style.display = "none";
+          }
+          else {
+            ticknums[i][j].style.display = "";
+          }
         }
       }
     }
