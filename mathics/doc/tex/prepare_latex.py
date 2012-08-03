3540bad69e64e08521a188da32ad62249adbbea3
Jan Pöschko <jan@poeschko.com>
2012-08-03 21:13:33 +0200

    create more Asymptote stub files

--- mathics/doc/tex/prepare_latex.py
+++ mathics/doc/tex/prepare_latex.py
@@ -1,9 +1,11 @@
 """
-Obviously, pdflatex fails (goes into an infinite loop) when there are no mathics_*_.tex files for the Asymptote graphics. That's why we create a 99 such files before running pdflatex.
+Obviously, pdflatex fails (goes into an infinite loop)
+when there are no mathics_*_.tex files for the Asymptote graphics.
+That's why we create 199 such files before running pdflatex.
 """
 
 def main():
-    for index in range(1, 100):
+    for index in range(1, 200):
         with open('mathics_%d_.tex' % index, 'wt') as img:
             img.write('-Graphics-')
 
