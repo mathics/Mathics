44e440f9595870cd0f47f5cea4b27fdc4af15da2
Angus Griffith <16sn6uv@gmail.com>
2012-08-26 03:23:49 +1000

    RSolve boundary conditions

--- mathics/builtin/recurrence.py
+++ mathics/builtin/recurrence.py
@@ -19,8 +19,18 @@ class RSolve(Builtin):
     >> RSolve[a[n] == a[n+1], a[n], n]
      = {{a[n] -> C[0]}}
 
-    >> RSolve[a[n + 2] == 3 a[n], a, n]
-    = {{a -> (Function[{n}, C[1] (-Sqrt[3]) ^ n + C[0] 3 ^ (n / 2)])}}
+    No boundary conditions gives two general paramaters.
+    >> RSolve[{a[n + 2] == a[n]}, a, n]
+     = {{a -> (Function[{n}, C[1] -1 ^ n + C[0]])}}
+
+    One boundary condition.
+    >> RSolve[{a[n + 2] == a[n], a[0] == 1}, a, n]
+     = {{a -> (Function[{n}, 1 - C[1] + C[1] -1 ^ n])}}
+
+    Two boundary conditions.
+    >> RSolve[{a[n + 2] == a[n], a[0] == 1, a[1] == 4}, a, n]
+     = {{a -> (Function[{n}, 5 / 2 - 3 -1 ^ n / 2])}}
+
     """
 
     messages = {
@@ -30,14 +40,20 @@ class RSolve(Builtin):
         'dsvar': '`1` cannot be used as a variable.',
     }
 
-    def apply(self, eqn, a, n, evaluation):
-        'RSolve[eqn_, a_, n_]'
+    def apply(self, eqns, a, n, evaluation):
+        'RSolve[eqns_, a_, n_]'
 
-        #TODO: Handle initial conditions
-        
-        if eqn.get_head_name() != 'Equal':
-            evaluation.message('RSolve', 'deqn', eqn)
+        #TODO: Do this with rules
+        if not eqns.has_form('List', None):
+            eqns = Expression('List', eqns)
+
+        if len(eqns.leaves) == 0:
             return
+
+        for eqn in eqns.leaves:
+            if eqn.get_head_name() != 'Equal':
+                evaluation.message('RSolve', 'deqn', eqn)
+                return
 
         if (n.is_atom() and not n.is_symbol()) or \
           n.get_head_name() in ('Plus', 'Times', 'Power') or \
@@ -54,20 +70,41 @@ class RSolve(Builtin):
             function_form = Expression('List', n)
 
         if func.is_atom() or len(func.leaves) != 1:
-            evaluation.message('RSolve', 'dsfun', y)
+            evaluation.message('RSolve', 'dsfun', a)
 
         if n not in func.leaves:
             evaluation.message('DSolve', 'deqx')
 
-        left, right = eqn.leaves
-        eqn = Expression('Plus', left, Expression('Times', -1, right)).evaluate(evaluation)
+        # Seperate relations from conditions
+        conditions = {}
+        def is_relation(eqn):
+            left, right = eqn.leaves
+            for l,r in [(left, right), (right, left)]:
+                if left.get_head_name() == func.get_head_name() and len(left.leaves) == 1 \
+                  and isinstance(l.leaves[0].to_python(), int) and r.is_numeric():
+                    conditions[l.leaves[0].to_python()] = r.to_sympy()
+                    return False
+            return True
+        relation = filter(is_relation, eqns.leaves)[0]
 
-        sym_eq = eqn.to_sympy(converted_functions = set([func.get_head_name()]))
+        left, right = relation.leaves
+        relation = Expression('Plus', left, Expression('Times', -1, right)).evaluate(evaluation)
+
+        sym_eq = relation.to_sympy(converted_functions = set([func.get_head_name()]))
         sym_n = sympy.symbols(str(sympy_symbol_prefix + n.name))
         sym_func = sympy.Function(str(sympy_symbol_prefix + func.get_head_name())) (sym_n)
 
+        sym_conds = {}
+        for cond in conditions:
+            sym_conds[sympy.Function(str(sympy_symbol_prefix + func.get_head_name()))(cond)] = conditions[cond]
+
         try:
-            sym_result = sympy.rsolve(sym_eq, sym_func)
+            # Sympy raises error when given empty conditions (issue 1511 on sympy github)
+            if sym_conds != {}:
+                sym_result = sympy.rsolve(sym_eq, sym_func, sym_conds)
+            else:
+                sym_result = sympy.rsolve(sym_eq, sym_func)
+
             if not isinstance(sym_result, list):
                 sym_result = [sym_result]
         except ValueError as ve:
