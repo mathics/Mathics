4b00a5768fa2507e4c2459cdf5d691b0a66e1e72
Angus Griffith <16sn6uv@gmail.com>
2012-10-25 23:41:23 +1100

    add benchmarks for Random and Plotting

--- mathics/benchmark/Plus.m
+++ mathics/benchmark/Plus.m
@@ -13,7 +13,7 @@ Timing[Plus @@ l2][[1]]
 Timing[Plus @@ l2][[1]]
 
 (* Addition of Boolean Integers *)
-l1 = RandomInteger[{0,1}, 100];
+l1 = RandomInteger[{0,1}, 200];
 Timing[Plus @@ l1][[1]]
 Timing[Plus @@ l1][[1]]
 Timing[Plus @@ l1][[1]]
@@ -21,7 +21,7 @@ Timing[Plus @@ l1][[1]]
 Timing[Plus @@ l1][[1]]
 
 (* Addition of Small Integers *)
-l1 = RandomInteger[{0,100}, 100];
+l1 = RandomInteger[{0,100}, 200];
 Timing[Plus @@ l1][[1]]
 Timing[Plus @@ l1][[1]]
 Timing[Plus @@ l1][[1]]
@@ -29,7 +29,29 @@ Timing[Plus @@ l1][[1]]
 Timing[Plus @@ l1][[1]]
 
 (* Addition of Large Integers *)
-l1 = RandomInteger[{0,10^6}, 100];
+l1 = RandomInteger[{0,10^6}, 200];
+Timing[Plus @@ l1][[1]]
+Timing[Plus @@ l1][[1]]
+Timing[Plus @@ l1][[1]]
+Timing[Plus @@ l1][[1]]
+Timing[Plus @@ l1][[1]]
+
+(* Addition of Small Floats *)
+l1 = RandomReal{0, 1}, 200];
+l2 = RandomReal{-1, 1}, 200];
+Timing[Plus @@ l1][[1]]
+Timing[Plus @@ l1][[1]]
+Timing[Plus @@ l1][[1]]
+Timing[Plus @@ l1][[1]]
+Timing[Plus @@ l1][[1]]
+Timing[Plus @@ l2][[1]]
+Timing[Plus @@ l2][[1]]
+Timing[Plus @@ l2][[1]]
+Timing[Plus @@ l2][[1]]
+Timing[Plus @@ l2][[1]]
+
+(* Addition of Large Floats *)
+l1 = RandomReal[{-10^10, 10^10}, 200];
 Timing[Plus @@ l1][[1]]
 Timing[Plus @@ l1][[1]]
 Timing[Plus @@ l1][[1]]
@@ -38,5 +60,6 @@ Timing[Plus @@ l1][[1]]
 
 (* Addition of Exact Complex Numbers *)
 
+(* Addition of Inexact Complex Numbers *)
 
 (* Symbolic Additions *)
---
+++ mathics/benchmark/Random.m
@@ -0,0 +1,15 @@
+(* RandomInteger *)
+Timing[RandomInteger[1, 100]][[1]]
+Timing[RandomInteger[1, 100]][[1]]
+Timing[RandomInteger[1, 100]][[1]]
+Timing[RandomInteger[{-1, 1}, 100]][[1]]
+Timing[RandomInteger[{-1, 1}, 100]][[1]]
+Timing[RandomInteger[{-1, 1}, 100]][[1]]
+
+(* RandomReal *)
+Timing[RandomReal[1, 100]][[1]]
+Timing[RandomReal[1, 100]][[1]]
+Timing[RandomReal[1, 100]][[1]]
+Timing[RandomReal[{-1, 1}, 100]][[1]]
+Timing[RandomReal[{-1, 1}, 100]][[1]]
+Timing[RandomReal[{-1, 1}, 100]][[1]]
