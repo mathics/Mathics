0dd4b49a56455b85ada118d8154fc063a6a5d0f1
Angus Griffith <16sn6uv@gmail.com>
2012-10-26 19:51:40 +1100

    make Plus benchmarks harder

--- mathics/benchmark/Plus.m
+++ mathics/benchmark/Plus.m
@@ -13,7 +13,7 @@ Timing[Plus @@ l2][[1]]
 Timing[Plus @@ l2][[1]]
 
 (* Addition of Boolean Integers *)
-l1 = RandomInteger[{0,1}, 200];
+l1 = RandomInteger[{0,1}, 1000];
 Timing[Plus @@ l1][[1]]
 Timing[Plus @@ l1][[1]]
 Timing[Plus @@ l1][[1]]
@@ -21,7 +21,7 @@ Timing[Plus @@ l1][[1]]
 Timing[Plus @@ l1][[1]]
 
 (* Addition of Small Integers *)
-l1 = RandomInteger[{0,100}, 200];
+l1 = RandomInteger[{0,100}, 1000];
 Timing[Plus @@ l1][[1]]
 Timing[Plus @@ l1][[1]]
 Timing[Plus @@ l1][[1]]
@@ -29,7 +29,7 @@ Timing[Plus @@ l1][[1]]
 Timing[Plus @@ l1][[1]]
 
 (* Addition of Large Integers *)
-l1 = RandomInteger[{0,10^6}, 200];
+l1 = RandomInteger[{0,10^6}, 1000];
 Timing[Plus @@ l1][[1]]
 Timing[Plus @@ l1][[1]]
 Timing[Plus @@ l1][[1]]
@@ -37,8 +37,8 @@ Timing[Plus @@ l1][[1]]
 Timing[Plus @@ l1][[1]]
 
 (* Addition of Small Floats *)
-l1 = RandomReal{0, 1}, 200];
-l2 = RandomReal{-1, 1}, 200];
+l1 = RandomReal[{0, 1}, 1000];
+l2 = RandomReal[{-1, 1}, 1000];
 Timing[Plus @@ l1][[1]]
 Timing[Plus @@ l1][[1]]
 Timing[Plus @@ l1][[1]]
@@ -51,7 +51,7 @@ Timing[Plus @@ l2][[1]]
 Timing[Plus @@ l2][[1]]
 
 (* Addition of Large Floats *)
-l1 = RandomReal[{-10^10, 10^10}, 200];
+l1 = RandomReal[{-10^10, 10^10}, 1000];
 Timing[Plus @@ l1][[1]]
 Timing[Plus @@ l1][[1]]
 Timing[Plus @@ l1][[1]]
