67663ccd3392e877171f17507aa3b45006d80838
Angus Griffith <16sn6uv@gmail.com>
2012-11-11 17:08:38 +1100

    add Travis-CI testing

--- mathics/test.py
+++ mathics/test.py
@@ -187,6 +187,7 @@ def test_all(quiet=False):
             pickle.dump(output_tex, output_tex_file, 0)
     else:
         print '\nFAILED'
+        return sys.exit(1)      # Travis-CI knows the tests have failed
         
 def write_latex():
     print "Load data"
