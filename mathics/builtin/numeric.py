4d6e094be8608d47f2b1a70ec11aeeb18fe00269
mr.Shu <mr@shu.io>
2012-12-08 16:13:26 +0100

    fix not existing variables

--- mathics/builtin/numeric.py
+++ mathics/builtin/numeric.py
@@ -420,10 +420,15 @@ class BaseForm(Builtin):
 
         from mathics.core.expression import from_python
         base = n.get_int_value()
+        print expr.get_int_value()
 
         if base <= 0:
             evaluation.message('BaseForm', 'base', expr, n)
             return
+
+        if expr.get_int_value() is None:
+            return from_python(expr)
+
 
         if isinstance(expr, Real):
             (num, real) = divmod(expr.get_real_value(), 1)
