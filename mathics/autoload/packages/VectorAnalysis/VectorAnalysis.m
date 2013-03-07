dd8d7105b31f325e8c0d70d307740ab220373291
Angus Griffith <16sn6uv@gmail.com>
2013-03-07 12:46:18 +1100

    implement Coordinates, Paramaters, CoordinatRanges and ParamaterRanges

--- mathics/autoload/packages/VectorAnalysis/VectorAnalysis.m
+++ mathics/autoload/packages/VectorAnalysis/VectorAnalysis.m
@@ -11,26 +11,26 @@ coordinate transformations between orthogonal coordinate systems.
 
 BeginPackage["VectorAnalysis`"]
 
-$CoordSysList = {Cartesian};
+$CoordSysList = {Cartesian, Spherical, Cylindrical};
 
-$CoordinateSystem = Cartesian
+CoordinateSystem = Cartesian
 
 IsVecQ[v_] := Length[v] == 3 && VectorQ[v];
 
-DotProduct[v1_?IsVecQ, v2_?IsVecQ, coordsys_:$CoordinateSystem] :=
+DotProduct[v1_?IsVecQ, v2_?IsVecQ, coordsys_:CoordinateSystem] :=
     Module[{c1, c2},
     c1 = CoordinatesToCartesian[v1, coordsys];
     c2 = CoordinatesToCartesian[v2, coordsys];
     Apply[Plus, c1 * c2]]
 
-CrossProduct[v1_?IsVecQ, v2_?IsVecQ, coordsys_:$CoordinateSystem] :=
+CrossProduct[v1_?IsVecQ, v2_?IsVecQ, coordsys_:CoordinateSystem] :=
     Module[{c1, c2},
     c1 = CoordinatesToCartesian[v1, coordsys];
     c2 = CoordinatesToCartesian[v2, coordsys];
     CoordinatesFromCartesian[Det[{IdentityMatrix[3], c1, c2}]]]
 
 ScalarTripleProduct[v1_?IsVecQ, v2_?IsVecQ, v3_?IsVecQ, 
-        coordsys_:$CoordinateSystem] := 
+        coordsys_:CoordinateSystem] := 
     Module[{c1, c2, c3},
     c1 = CoordinatesToCartesian[v1, coordsys];
     c2 = CoordinatesToCartesian[v2, coordsys];
@@ -38,7 +38,7 @@ ScalarTripleProduct[v1_?IsVecQ, v2_?IsVecQ, v3_?IsVecQ,
     Det[{c1, c2, c3}]]
 
 (* TODO: Other coordinate systems *)
-CoordinatesToCartesian[pt_?IsVecQ, coordsys_:$CoordinateSystem] := 
+CoordinatesToCartesian[pt_?IsVecQ, coordsys_:CoordinateSystem] := 
     Module[{v1 = pt[[1]], v2 = pt[[2]], v3 = pt[[3]]}, 
         Switch[coordsys,
             Cartesian, {v1, v2, v3},
@@ -47,7 +47,7 @@ CoordinatesToCartesian[pt_?IsVecQ, coordsys_:$CoordinateSystem] :=
         ]
     ]
 
-CoordinatesFromCartesian[pt_?IsVecQ, coordsys_:$CoordinateSystem] :=
+CoordinatesFromCartesian[pt_?IsVecQ, coordsys_:CoordinateSystem] :=
     Module[{v1 = pt[[1]], v2 = pt[[2]], v3 = pt[[3]]}, 
         Switch[coordsys,
             Cartesian, {v1, v2, v3},
@@ -56,10 +56,47 @@ CoordinatesFromCartesian[pt_?IsVecQ, coordsys_:$CoordinateSystem] :=
         ]
     ]
 
+Coordinates::invalid = "`1` is not a valid coordinate system specification.";
+
+Coordinates[] := Coordinates[CoordinateSystem];
+Coordinates[Cartesian] ^= {Xx, Yy, Zz};
+Coordinates[Spherical] ^= {Rr, Ttheta, Pphi};
+Coordinates[Cylindrical] ^= {Rr, Ttheta, Zz};
+
+Paramaters[] := Paramaters[CoordinateSystem];
+Paramaters[Cartesian] ^= {};
+Paramaters[Spherical] ^= {};
+Paramaters[Cylindrical] ^= {};
+
+CoordinateRanges[] := CoordinateRanges[CoordinateSystem];
+CoordinateRanges[Cartesian] ^= {-Infinity < Xx < Infinity, -Infinity < Yy < Infinity, -Infinity < Zz <Infinity};
+CoordinateRanges[Spherical] ^=   {0 <= Rr < Infinity, 0 <= Ttheta <= Pi, -Pi < Pphi <= Pi};
+CoordinateRanges[Cylindrical] ^= {0 <= Rr < Infinity, -Pi < Ttheta <= Pi, -Infinity < Zz <Infinity};
+
+ParamaterRanges[] := ParamatersRanges[CoordinateSystem];
+ParamaterRanges[Cartesian] ^= Null;
+ParamaterRanges[Spherical] ^= Null;
+ParamaterRanges[Cylindrical] ^= Null;
+
+SetCoordinates[coordsys_Symbol] :=
+    If[MemberQ[$CoordSysList, coordsys],
+        Unprotect[CoordinateSystem];
+        CoordinateSystem = coordsys;
+        Protect[CoordinateSystem];
+        Apply[coordsys, Coordinates[coordsys]],
+        Message[Coordinates::invalid, coordsys];
+        $Failed]
+
 Attributes[DotProduct] = {ReadProtected, Protected};
 Attributes[CrossProduct] = {ReadProtected, Protected};
 Attributes[ScalarTripleProduct] = {ReadProtected, Protected};
 Attributes[CoordinatesToCartesian] = {ReadProtected, Protected};
 Attributes[CoordinatesFromCartesian] = {ReadProtected, Protected};
+Attributes[SetCoordinates] = {ReadProtected, Protected};
+Attributes[CoordinateSystem] = {ReadProtected, Protected};
+Attributes[Coordinates] = {ReadProtected, Protected};
+Attributes[Paramaters] = {ReadProtected, Protected};
+Attributes[CoordinateRanges] = {ReadProtected, Protected};
+Attributes[ParamaterRanges] = {ReadProtected, Protected};
 
 EndPackage[]
