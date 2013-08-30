8a2d0309ff66705d5f264ea8d032ffb6656e9879
Ben Jones <benj2579@gmail.com>
2013-08-30 01:01:24 +0100

    Add Through[]

--- mathics/builtin/structure.py
+++ mathics/builtin/structure.py
@@ -658,3 +658,34 @@ class Operate(Builtin):
         e.head = Expression(p, e.head)
 
         return expr
+
+
+class Through(Builtin):
+    """
+    <dl>
+    <dt>'Through[$p$[$f$][$x$]]'
+    <dd>gives $p$[$f$[$x$]].
+    </dl>
+
+    >> Through[f[g][x]]
+     = f[g[x]]
+    >> Through[p[f, g][x]]
+     = p[f[x], g[x]]
+
+    #> Through[p[f, g][x, y]]
+     = p[f[x, y], g[x, y]]
+    #> Through[p[f, g][]]
+     = p[f[], g[]]
+    #> Through[p[f, g]]
+     = Through[p[f, g]]
+    #> Through[f[][x]]
+     = f[]
+    """
+
+    def apply(self, p, args, x, evaluation):
+        'Through[p_[args___][x___]]'
+
+        items = []
+        for leaf in args.get_sequence():
+            items.append(Expression(leaf, *x.get_sequence()))
+        return Expression(p, *items)
