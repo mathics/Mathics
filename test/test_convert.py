8b09acf59947ab15b94a40739ddf0dd39b3fafe8
Angus Griffith <16sn6uv@gmail.com>
2013-09-24 20:55:15 +1000

    replace unittest with unittest2

--- test/test_convert.py
+++ test/test_convert.py
@@ -1,4 +1,4 @@
-import unittest
+import unittest2 as unittest
 import sympy
 import mathics
 import random
--- test/test_parser.py
+++ test/test_parser.py
@@ -1,4 +1,4 @@
-import unittest
+import unittest2 as unittest
 import sys
 import random
 from mathics import *
