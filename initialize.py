3dc506e1368a590daf47926819ac53245abdc5de
Ben Jones <benj2579@gmail.com>
2014-01-03 17:21:51 +0000

    Create a dummy initialize.py which says to run "setup.py initialize"
    This is to help users migrating from 0.5

---
+++ initialize.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import sys
+
+sys.stderr.write(
+    """Since Mathics 0.5, the functionality of initialize.py has been moved
+into setup.py. Please run:
+
+  {python} setup.py initialize
+
+to initialize the database.
+""".format(python=sys.executable))
+sys.exit(1)
