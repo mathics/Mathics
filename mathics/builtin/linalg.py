1da0d72f8ab7cda73ea96118da27f272290f9a7f
Angus Griffith <16sn6uv@gmail.com>
2015-07-11 00:02:23 +1000

    cleanup VectorAngle

--- mathics/builtin/linalg.py
+++ mathics/builtin/linalg.py
@@ -109,6 +109,12 @@ class VectorAngle(Builtin):
 
     >> VectorAngle[{1, 2}, {3, 1}]
      = Pi / 4
+
+    >> VectorAngle[{1, 1, 0}, {1, 0, 1}]
+     = Pi / 3
+
+    #> VectorAngle[{0, 1}, {0, 1}]
+     = 0
     """
 
     rules = {
