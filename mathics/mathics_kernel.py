982165cc59e7106938aec886712b841b27bf34d5
mauricio matera <matera@fisica.unlp.edu.ar>
2015-09-16 12:36:16 +0200

    doing pep8 compiling 	modified:   mathics/mathics_kernel.py

--- mathics/mathics_kernel.py
+++ mathics/mathics_kernel.py
@@ -2,7 +2,7 @@ from __future__ import print_function
 
 from metakernel import MetaKernel, ProcessMetaKernel, REPLWrapper, u
 from IPython.display import Image, SVG
-from IPython.display import Latex,HTML
+from IPython.display import Latex, HTML
 
 import subprocess
 import os
@@ -28,7 +28,7 @@ class MathicsKernel(ProcessMetaKernel):
     }
 
     _initstring = """
-$MyPrePrint:=Module[{fn,res},    
+$MyPrePrint:=Module[{fn,res}, 
 If[#1 === Null, res=\"null:\",
 Switch[Head[#1],
           String,
@@ -46,7 +46,7 @@ Switch[Head[#1],
             Export[fn,#1,"wav"];
             res=\"sound:\"<>fn<>\":\"<>\"- sound -\",
           _,            
-	    texstr=StringReplace[ToString[TeXForm[#1]],\"\\n\"->\" \"];
+            texstr=StringReplace[ToString[TeXForm[#1]],\"\\n\"->\" \"];
             res=\"tex:\"<>ToString[StringLength[texstr]]<>\":\"<> texstr<>\":\"<>ToString[InputForm[#1]]
        ]
        ];
@@ -55,9 +55,9 @@ Switch[Head[#1],
 $DisplayFunction=Identity;
 """
 
-    _session_dir=""
+    _session_dir = ""
     _first = True
-    initfilename=""
+    initfilename = ""
     _banner = None
 
     @property
@@ -78,25 +78,25 @@ $DisplayFunction=Identity;
 
         self._first = True
         self._session_dir = tempfile.mkdtemp()
-        self._initstring=self._initstring.replace("{sessiondir}",self._session_dir)
-        self.initfilename=self._session_dir+'/init.m'
+        self._initstring = self._initstring.replace("{sessiondir}", self._session_dir)
+        self.initfilename = self._session_dir + '/init.m'
         print(self.initfilename)
-        f= open(self.initfilename,'w')
+        f = open(self.initfilename, 'w')
         f.write(self._initstring)
         f.close()
         if self.language_info['exec'] == 'mathics':
-            replwrapper=REPLWrapper("mathics --colors NOCOLOR", orig_prompt, change_prompt,
-                           prompt_emit_cmd=prompt_cmd,echo=True)
+            replwrapper = REPLWrapper("mathics --colors NOCOLOR", orig_prompt, change_prompt,
+                           prompt_emit_cmd=prompt_cmd, echo=True)
         else:
-            replwrapper=REPLWrapper("math ", orig_prompt, change_prompt,
-                           prompt_emit_cmd=prompt_cmd,echo=True)
+            replwrapper = REPLWrapper("math ", orig_prompt, change_prompt,
+                           prompt_emit_cmd=prompt_cmd, echo=True)
         return replwrapper
 
     def do_execute_direct(self, code):
         if(self._first):
-            super(MathicsKernel, self).do_execute_direct("Get[\""+ self.initfilename+"\"];$Line=0;")
-            self._first=False
-        #Processing multiline code        
+            super(MathicsKernel, self).do_execute_direct("Get[\"" + self.initfilename + "\"];$Line=0;")
+            self._first = False
+        # Processing multiline code
         codelines = code.splitlines()
         lastline = ""
         resp = ""
@@ -112,70 +112,70 @@ $DisplayFunction=Identity;
                 continue
             lastline = lastline + codeline
         code = lastline.strip()
-        if code == "" :
+        if code == "":
             return resp
         else:
-            if resp != "" :
+            if resp != "":
                 print(resp)
-        #Processing last valid line
-        resp = super(MathicsKernel, self).do_execute_direct("$MyPrePrint["+code+"]")
+            # Processing last valid line
+        resp = super(MathicsKernel, self).do_execute_direct("$MyPrePrint[" + code + "]")
 
-        lineresponse=resp.splitlines()
-        outputfound=False
-        outputtext=""
-        for linnum,liner in enumerate(lineresponse):
-            if not outputfound and liner[:4]  == "Out[" :
-                outputfound=True
-                for pos in xrange(len(liner)-4):
-                    if liner[pos+4]=='=':
-                        outputtext=liner[(pos + 6):]
+        lineresponse = resp.splitlines()
+        outputfound = False
+        outputtext = ""
+        for linnum, liner in enumerate(lineresponse):
+            if not outputfound and liner[:4] == "Out[":
+                outputfound = True
+                for pos in range(len(liner) - 4):
+                    if liner[pos + 4] == '=':
+                        outputtext = liner[(pos + 6):]
                 continue
             if outputfound:
                 if liner == u' ':
-                    if outputtext[-1] == '\\' :  #and lineresponse[linnum + 1] == '>':
+                    if outputtext[-1] == '\\':  # and lineresponse[linnum + 1] == '>':
                         outputtext = outputtext[:-1]
                         lineresponse[linnum + 1] = lineresponse[linnum + 1][4:]
-                        continue                      
+                        continue
                 outputtext = outputtext + liner
             else:
                 print(liner)
-        if(outputtext[:5]=='null:'):
+        if(outputtext[:5] == 'null:'):
             return ""
-        if(outputtext[:4]=='svg:'):            
-	    for p in xrange( len(outputtext) - 4 ):
-                pp=p+4
-                if outputtext[pp]==':':
+        if(outputtext[:4] == 'svg:'):
+            for p in range(len(outputtext) - 4):
+                pp = p + 4
+                if outputtext[pp] == ':':
                     self.Display(SVG(outputtext[4:pp]))
-                    return outputtext[(pp+1):]
-        if(outputtext[:6]=='image:'):
-	    for p in xrange( len(outputtext) - 6 ):
-                pp=p+6
-                if outputtext[pp]==':':
+                    return outputtext[(pp + 1):]
+        if(outputtext[:6] == 'image:'):
+            for p in range(len(outputtext) - 6):
+                pp = p + 6
+                if outputtext[pp] == ':':
                     print(outputtext[6:pp])
                     self.Display(Image(outputtext[6:pp]))
-                    return outputtext[(pp+1):]
-        if(outputtext[:6]=='sound:'): 
-	    for p in xrange( len(outputtext) - 6 ):
-                pp=p+6
-                if outputtext[pp]==':':
-                    htmlstr="<audio controls> <source src=\"file://"
-                    htmlstr=htmlstr+outputtext[6:pp]
-                    htmlstr=htmlstr+ "\" type=\"audio/wav\">"
-                    htmlstr=htmlstr+"Your browser does not support the audio element."
-                    htmlstr=htmlstr+"</audio>"
+                    return outputtext[(pp + 1):]
+        if(outputtext[:6] == 'sound:'):
+            for p in range(len(outputtext) - 6):
+                pp = p + 6
+                if outputtext[pp] == ':':
+                    htmlstr = "<audio controls> <source src=\"file://"
+                    htmlstr = htmlstr + outputtext[6:pp]
+                    htmlstr = htmlstr + "\" type=\"audio/wav\">"
+                    htmlstr = htmlstr + "Your browser does not support the audio element."
+                    htmlstr = htmlstr + "</audio>"
                     self.Display(HTML(htmlstr))
                     print(htmlstr)
                     return outputtext[(pp + 1):]
-        if (outputtext[:7]=='string:'):
+        if (outputtext[:7] == 'string:'):
             return outputtext[7:]
-        if (outputtext[:4]=='tex:'):
-	    for p in xrange( len(outputtext) - 4 ):
-                pp=p+4
-                if outputtext[pp]==':':
-                    lentex=int(outputtext[4:(pp)])                    
-                    self.Display(Latex('$' + outputtext[(pp+1):(pp+lentex+1)] +'$'))
-                    return outputtext[(pp + lentex+2):]
-                
+        if (outputtext[:4] == 'tex:'):
+            for p in range(len(outputtext) - 4):
+                pp = p + 4
+                if outputtext[pp] == ':':
+                    lentex = int(outputtext[4:pp])
+                    self.Display(Latex('$' + outputtext[(pp + 1):(pp + lentex + 1)] + '$'))
+                    return outputtext[(pp + lentex + 2):]
+
 #        if self.plot_settings.get('backend', None) == 'inline':
 #            plot_dir = tempfile.mkdtemp()
 #            self._make_figs(plot_dir)
@@ -189,7 +189,6 @@ $DisplayFunction=Identity;
 #                    self.Display(im)
 #                except Exception as e:
 #                    self.Error(e)
-        
 
     def get_kernel_help_on(self, info, level=0, none_on_fail=False):
         obj = info.get('help_obj', '')
@@ -226,18 +225,3 @@ $DisplayFunction=Identity;
 if __name__ == '__main__':
     from IPython.kernel.zmq.kernelapp import IPKernelApp
     IPKernelApp.launch_instance(kernel_class=MathicsKernel)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
