2b60a7efb7a7dc80718d6797ae1a8dfb591e7359
Bernhard Liebl <Bernhard.Liebl@gmx.org>
2016-04-27 23:56:29 +0200

    added ipywidgets to required packages

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ else:
 
 # General Requirements
 INSTALL_REQUIRES += ['sympy==1.0', 'django >= 1.8, < 1.9', 'ply==3.8',
-                     'mpmath>=0.19', 'python-dateutil', 'colorama', 'six>=1.10']
+                     'mpmath>=0.19', 'python-dateutil', 'colorama', 'six>=1.10', 'ipywidgets']
 
 
 def subdirs(root, file='*.*', depth=10):
