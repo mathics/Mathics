06526c458206a7754a572f7ef3734d9e62d518b8
Bernhard Liebl <Bernhard.Liebl@gmx.org>
2016-05-06 07:30:10 +0200

    removed PatternDispatcher

--- mathics/builtin/manipulate.py
+++ mathics/builtin/manipulate.py
@@ -4,14 +4,12 @@
 from __future__ import unicode_literals
 from __future__ import absolute_import
 
-from mathics.core.expression import String
+from mathics.core.expression import String, strip_context
 from mathics import settings
 from mathics.core.evaluation import Evaluation
 
 from mathics.builtin.base import Builtin
 from mathics.core.expression import Expression, Symbol, Integer, from_python
-
-import six
 
 try:
     from ipykernel.kernelbase import Kernel
@@ -53,70 +51,119 @@ def _interactive(interact_f, kwargs_widgets):
     return container
 
 
-def _strip_namespace(name):
-    return name.split('`')[-1]  # e.g. "Global`x" will turn into "x"
-
-
-def _manipulate_label(x):
-    if isinstance(x, String):
-        return x.get_string_value()
-    elif isinstance(x, Symbol):
-        return _strip_namespace(x.get_name())
-    else:
-        return str(x)
-
-
-class PatternDispatcher:
-    '''A pattern matcher similar to the one for BuiltIns, but usable in any context (e.g. locally)'''
-
-    def __init__(self):
-        from mathics.core.parser import parse_builtin_rule
-        from mathics.builtin.patterns import Pattern
-
-        self._patterns = []
-        for name in dir(self):
-            if name.startswith('apply_'):
-                function = getattr(self, name)
-                self._patterns.append((Pattern.create(
-                    parse_builtin_rule(function.__doc__)), function))
-
-    def dispatch(self, expr, evaluation):
-        from mathics.builtin.patterns import StopGenerator
-
-        class StopGenerator_MatchQ(StopGenerator):
-            pass
-
-        try:
-            for pattern, function in self._patterns:
-                def yield_func(vars, rest):
-                    function(**dict((_strip_namespace(key), value) for (key, value) in vars.items()))
-                    raise StopGenerator_MatchQ(Symbol("True"))
-
-                pattern.match(yield_func, expr, {}, evaluation)
-        except StopGenerator_MatchQ:
-            return True
-
-        return False
-
-
 class IllegalWidgetArguments(Exception):
     def __init__(self, var):
         super(IllegalWidgetArguments, self).__init__()
         self.var = var
 
 
-class Manipulations(PatternDispatcher):
-    def __init__(self, evaluation):
-        if six.PY2:
-            PatternDispatcher.__init__(self)
-        else:
-            super(Manipulations, self).__init__()
-        self._evaluation = evaluation
+class ManipulateParameter(Builtin): # parses one Manipulate[] parameter spec, e.g. {x, 1, 2}, see _WidgetInstantiator
+    context = 'System`Private`'
+
+    rules = {
+        # detect x and {x, default} and {x, default, label}.
+        'System`Private`ManipulateParameter[{s_Symbol, r__}]':
+            'System`Private`ManipulateParameter[{Symbol -> s, Label -> s}, {r}]',
+        'System`Private`ManipulateParameter[{{s_Symbol, d_}, r__}]':
+            'System`Private`ManipulateParameter[{Symbol -> s, Label -> s, Default -> d}, {r}]',
+        'System`Private`ManipulateParameter[{{s_Symbol, d_, l_}, r__}]':
+            'System`Private`ManipulateParameter[{Symbol -> i, Label -> l, Default -> d}, {r}]',
+
+        # detect different kinds of widgets. on the use of the duplicate key "Default ->", see _WidgetInstantiator.add()
+        'System`Private`ManipulateParameter[var_, {min_?RealNumberQ, max_?RealNumberQ}]':
+            'Join[{Type -> "Continuous", Minimum -> min, Maximum -> max, Default -> min}, var]',
+        'System`Private`ManipulateParameter[var_, {min_?RealNumberQ, max_?RealNumberQ, ds_?RealNumberQ}]':
+            'Join[{Type -> "Discrete", Minimum -> min, Maximum -> max, Step -> ds, Default -> min}, var]',
+        'System`Private`ManipulateParameter[var_, {opt_List}] /; Length[opt] > 0':
+            'Join[{Type -> "Options", Options -> opt, Default -> Part[opt, 1]}, var]'
+    }
+
+
+def _manipulate_label(x):  # gets the label that is displayed for a symbol or name
+    if isinstance(x, String):
+        return x.get_string_value()
+    elif isinstance(x, Symbol):
+        return strip_context(x.get_name())
+    else:
+        return str(x)
+
+
+class _WidgetInstantiator():
+    # we do not want to have widget instances (like FloatSlider) get into the evaluation pipeline (e.g. via Expression
+    # or Atom), since there might be all kinds of problems with serialization of these widget classes.  therefore, the
+    # elegant recursive solution for parsing parameters (like in Table[]) is not feasible here; instead, we must create
+    # and use the widgets in one "transaction" here, without holding them in expressions or atoms.
+
+    def __init__(self):
         self._widgets = []  # the ipywidget widgets to control the manipulated variables
         self._parsers = {}  # lambdas to decode the widget values into Mathics expressions
 
+    def add(self, expression, evaluation):
+        expr = Expression('System`Private`ManipulateParameter', expression).evaluate(evaluation)
+        if expr.get_head_name() != 'System`List':  # if everything was parsed ok, we get a List
+            return False
+        # convert the rules given us by ManipulateParameter[] into a dict. note: duplicate keys
+        # will be overwritten, the latest one wins.
+        kwargs = {'evaluation': evaluation}
+        for rule in expr.leaves:
+            if rule.get_head_name() != 'System`Rule' or len(rule.leaves) != 2:
+                return False
+            kwargs[strip_context(rule.leaves[0].to_python()).lower()] = rule.leaves[1]
+        widget = kwargs['type'].get_string_value()
+        del kwargs['type']
+        getattr(self, '_add_%s_widget' % widget.lower())(**kwargs)  # create the widget
+        return True
+
     def get_widgets(self):
         return self._widgets
+
+    def build_callback(self, callback):
+        parsers = self._parsers
+
+        def new_callback(**kwargs):
+            callback(**dict((name, parsers[name](value)) for (name, value) in kwargs.items()))
+
+        return new_callback
+
+    def _add_continuous_widget(self, symbol, label, default, minimum, maximum, evaluation):
+        minval = minimum.to_python()
+        maxval = maximum.to_python()
+        if minval > maxval:
+            raise IllegalWidgetArguments(symbol)
+        else:
+            defval = min(max(default.to_python(), minval), maxval)
+            widget = FloatSlider(value=defval, min=minval, max=maxval)
+            self._add_widget(widget, symbol.get_name(), lambda x: from_python(x), label)
+
+    def _add_discrete_widget(self, symbol, label, default, minimum, maximum, step, evaluation):
+        imin_value = minimum.to_python()
+        imax_value = maximum.to_python()
+        di_value = step.to_python()
+        if imin_value > imax_value or di_value <= 0 or di_value > (imax_value - imin_value):
+            raise IllegalWidgetArguments(symbol)
+        else:
+            idefault_value = min(max(default.to_python(), imin_value), imax_value)
+            if all(isinstance(x, Integer) for x in [minimum, maximum, default, step]):
+                widget = IntSlider(value=idefault_value, min=imin_value, max=imax_value,
+                                   step=di_value)
+            else:
+                widget = FloatSlider(value=idefault_value, min=imin_value, max=imax_value,
+                                     step=di_value)
+            self._add_widget(widget, symbol.get_name(), lambda x: from_python(x), label)
+
+    def _add_options_widget(self, symbol, options, default, label, evaluation):
+        formatted_options = []
+        for i, option in enumerate(options.leaves):
+            data = evaluation.format_all_outputs(option)
+            formatted_options.append((data['text/plain'], i))
+
+        default_index = 0
+        for i, option in enumerate(options.leaves):
+            if option.same(default):
+                default_index = i
+
+        widget = ToggleButtons(options=formatted_options, value=default_index)
+        self._add_widget(widget, symbol.get_name(), lambda j: options.leaves[j], label)
 
     def _add_widget(self, widget, name, parse, label):
         if not widget.description:
@@ -124,89 +171,6 @@ class Manipulations(PatternDispatcher):
         widget._kwarg = name  # see _interactive() above
         self._parsers[name] = parse
         self._widgets.append(widget)
-
-    def build_callback(self, callback):
-        parsers = self._parsers
-        def new_callback(**kwargs):
-            callback(**dict((name, parsers[name](value)) for (name, value) in kwargs.items()))
-        return new_callback
-
-    def _add_min_max_var(self, i, imin, imax, idefault, ilabel):
-        imin_value = imin.to_python()
-        imax_value = imax.to_python()
-        if imin_value > imax_value:
-            raise IllegalWidgetArguments(i)
-        else:
-            idefault_value = min(max(idefault.to_python(), imin_value), imax_value)
-            widget = FloatSlider(value=idefault_value, min=imin_value, max=imax_value)
-            self._add_widget(widget, i.get_name(), lambda x: from_python(x), ilabel)
-
-    def _add_min_max_step_var(self, i, imin, imax, idefault, di, ilabel):
-        imin_value = imin.to_python()
-        imax_value = imax.to_python()
-        di_value = di.to_python()
-        if imin_value > imax_value or di_value <= 0 or di_value > (imax_value - imin_value):
-            raise IllegalWidgetArguments(i)
-        else:
-            idefault_value = min(max(idefault.to_python(), imin_value), imax_value)
-            if all(isinstance(x, Integer) for x in [imin, imax, idefault, di]):
-                widget = IntSlider(value=idefault_value, min=imin_value, max=imax_value,
-                                   step=di_value)
-            else:
-                widget = FloatSlider(value=idefault_value, min=imin_value, max=imax_value,
-                                     step=di_value)
-            self._add_widget(widget, i.get_name(), lambda x: from_python(x), ilabel)
-
-    def _add_discrete_options_var(self, symbol, options, idefault, ilabel):
-        formatted_options = []
-        for i, option in enumerate(options.leaves):
-            data = self._evaluation.format_all_outputs(option)
-            formatted_options.append((data['text/plain'], i))
-
-        default_index = 0
-        for i, option in enumerate(options.leaves):
-            if option.same(idefault):
-                default_index = i
-
-        widget = ToggleButtons(options=formatted_options, value=default_index)
-        self._add_widget(widget, symbol.get_name(), lambda i: options.leaves[i], ilabel)
-
-    def apply_min_max_var1(self, i, imin, imax):
-        '{i_Symbol, imin_?RealNumberQ, imax_?RealNumberQ}'
-        self._add_min_max_var(i, imin, imax, imin, i)
-
-    def apply_min_max_var2(self, i, imin, imax, idefault):
-        '{{i_Symbol, idefault_?RealNumberQ}, imin_?RealNumberQ, imax_?RealNumberQ}'
-        self._add_min_max_var(i, imin, imax, idefault, i)
-
-    def apply_min_max_var3(self, i, imin, imax, idefault, ilabel):
-        '{{i_Symbol, idefault_?RealNumberQ, ilabel_}, imin_?RealNumberQ, imax_?RealNumberQ}'
-        self._add_min_max_var(i, imin, imax, idefault, ilabel)
-
-    def apply_min_max_step_var1(self, i, imin, imax, di):
-        '{i_Symbol, imin_?RealNumberQ, imax_?RealNumberQ, di_?RealNumberQ}'
-        self._add_min_max_step_var(i, imin, imax, imin, di, i)
-
-    def apply_min_max_step_var2(self, i, imin, imax, di, idefault):
-        '{{i_Symbol, idefault_?RealNumberQ}, imin_?RealNumberQ, imax_?RealNumberQ, di_?RealNumberQ}'
-        self._add_min_max_step_var(i, imin, imax, idefault, di, i)
-
-    def apply_min_max_step_var3(self, i, imin, imax, di, idefault, ilabel):
-        '{{i_Symbol, idefault_?RealNumberQ, ilabel_}, imin_?RealNumberQ, imax_?RealNumberQ, di_?RealNumberQ}'
-        self._add_min_max_step_var(i, imin, imax, idefault, di, ilabel)
-
-    def apply_discrete_options_var1(self, i, options):
-        '{i_Symbol, options_List}'
-        if len(options.leaves) > 0:
-            self._add_discrete_options_var(i, options, options.leaves[0], i)
-
-    def apply_discrete_options_var2(self, i, options, idefault):
-        '{{i_Symbol, idefault_}, options_List}'
-        self._add_discrete_options_var(i, options, idefault, i)
-
-    def apply_discrete_options_var3(self, i, options, idefault, ilabel):
-        '{{i_Symbol, idefault_, ilabel_}, options_List}'
-        self._add_discrete_options_var(i, options, idefault, ilabel)
 
 
 class Manipulate(Builtin):
@@ -267,21 +231,20 @@ class Manipulate(Builtin):
 
     def apply(self, expr, args, evaluation):
         'Manipulate[expr_, args__]'
-
         if (not _jupyter) or (not Kernel.initialized()) or (Kernel.instance() is None):
             return evaluation.message('Manipulate', 'jupyter')
 
         if not _ipywidgets:
             return evaluation.message('Manipulate', 'noipywidget')
 
-        manip = Manipulations(evaluation)  # knows about the arguments and their widgets
+        instantiator = _WidgetInstantiator()  # knows about the arguments and their widgets
 
         for arg in args.get_sequence():
             try:
-                if not manip.dispatch(arg.evaluate(evaluation), evaluation):  # not a valid argument pattern?
+                if not instantiator.add(arg, evaluation):  # not a valid argument pattern?
                     return
             except IllegalWidgetArguments as e:
-                evaluation.message('Manipulate', 'widgetargs', _strip_namespace(str(e.var)))
+                evaluation.message('Manipulate', 'widgetargs', strip_context(str(e.var)))
 
         clear_output_callback = evaluation.clear_output_callback
         display_data_callback = evaluation.display_data_callback  # for pushing updates
@@ -300,9 +263,9 @@ class Manipulate(Builtin):
 
             evaluation.definitions.set_line_no(line_no)  # do not increment line_no for manipulate computations
 
-        widgets = manip.get_widgets()
+        widgets = instantiator.get_widgets()
         if len(widgets) > 0:
-            box = _interactive(manip.build_callback(callback), widgets)  # create the widget
+            box = _interactive(instantiator.build_callback(callback), widgets)  # create the widget
 
             # the following code is a boiled down version from IPython.core.formatters.IPythonDisplayFormatter.
             # note that '_ipython_display_' is a magic constant defined in print_method of IPythonDisplayFormatter.
