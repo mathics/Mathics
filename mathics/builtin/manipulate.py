7334a4f38239cebf67b247678ec925b05c71b3ea
Bernhard Liebl <Bernhard.Liebl@gmx.org>
2016-05-14 21:32:00 +0200

    always display errors during widget creation

--- mathics/builtin/manipulate.py
+++ mathics/builtin/manipulate.py
@@ -226,6 +226,7 @@ class Manipulate(Builtin):
     messages = {
         'jupyter': 'Manipulate[] only works inside a Jupyter notebook.',
         'noipywidget': 'Manipulate[] needs the ipywidgets module to work.',
+        'generr': 'Widget creation failed: ``',
         'widgetargs': 'Illegal variable range or step parameters for ``.'
     }
 
@@ -245,6 +246,9 @@ class Manipulate(Builtin):
                     return
             except IllegalWidgetArguments as e:
                 evaluation.message('Manipulate', 'widgetargs', strip_context(str(e.var)))
+            except Exception as e:
+                evaluation.message('Manipulate', 'generr', str(e))  # usually some Jupyter error
+                return Symbol('$Aborted')
 
         clear_output_callback = evaluation.clear_output_callback
         display_data_callback = evaluation.display_data_callback  # for pushing updates
