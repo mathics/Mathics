12c0ffc98c593983b993a92a1e54a5c1e1c604ff
Bernhard Liebl <Bernhard.Liebl@gmx.org>
2016-05-27 19:53:09 +0200

    IntegerString work in progress

--- mathics/builtin/integer.py
+++ mathics/builtin/integer.py
@@ -12,7 +12,7 @@ import sympy
 
 from mathics.builtin.base import Builtin, SympyObject, SympyFunction
 from mathics.core.convert import from_sympy
-from mathics.core.expression import Integer, String
+from mathics.core.expression import Integer, String, Expression
 
 
 class Floor(SympyFunction):
@@ -138,30 +138,117 @@ class IntegerLength(Builtin):
         return Integer(result)
 
 
-class IntegerString(Builtin):
-    _digits = [chr(i + ord('0')) for i in range(10)] + [chr(i + ord('a')) for i in range(26)]
+class BitLength(Builtin):
+    """
+    <dl>
+    <dt>'BitLength[$x$]'
+        <dd>gives the number of bits needed to represent $x$.
+    </dl>
+
+    >> BitLength[1023]
+     = 10
+    >> BitLength[100]
+     = 10
+    """
 
     def apply(self, n, evaluation):
-        'IntegerString[n_Integer]'
-        return String(str(abs(n.get_int_value())))
+        'BitLength[n_Integer]'
+        return Integer(n.get_int_value().bit_length())
 
-    def apply(self, n, b, evaluation):
+
+def _reverse_digits(number, base):
+    number = abs(number)
+    if number == 0:
+        yield 0
+    else:
+        while number > 0:
+            rest, digit = divmod(number, base)
+            yield digit
+            number = rest
+
+
+class IntegerString(Builtin):
+    rules = {
+        'IntegerString[n_Integer]': 'IntegerString[n, 10]'
+    }
+
+    list_of_symbols = [chr(i + ord('0')) for i in range(10)] +\
+                      [chr(i + ord('a')) for i in range(26)]
+
+    _python_builtin = {
+        10: lambda number: str(abs(number)),
+        8: lambda number: oct(abs(number)),
+        2: lambda number: bin(abs(number))[2:]
+    }
+
+    def _build_string(self, n, b):
+        builtin = IntegerString._python_builtin.get(b)
+        if builtin:
+            return builtin(n)
+        else:
+            list_of_symbols = IntegerString.list_of_symbols
+            if b > len(list_of_symbols) or b < 2:
+                return False
+            return ''.join(reversed([list_of_symbols[r] for r in _reverse_digits(n, b)]))
+
+    def apply_n(self, n, b, evaluation):
         'IntegerString[n_Integer, b_Integer]'
+        s = self._build_string(n.get_int_value(), b.get_int_value())
+        return String(s) if s else None
+
+    def apply_n_b_length(self, n, b, length, evaluation):
+        'IntegerString[n_Integer, b_Integer, length_Integer]'
+        s = self._build_string(n.get_int_value(), b.get_int_value())
+        if not s:
+            return
+        pad_length = length.get_int_value() - len(s)
+        if pad_length <= 0:
+            return String(s[-pad_length:])
+        else:
+            return String('0' * pad_length + s)
+
+
+class IntegerDigits(Builtin):
+    def apply_n_b(self, n, b, evaluation):
+        'IntegerDigits[n_Integer, b_Integer]'
         base = b.get_int_value()
+        if base < 2:
+            return  # error
+        digits = [Integer(d) for d in reversed(_reverse_digits(n.get_int_value(), base))]
+        return Expression('List', *digits)
 
-        digits = IntegerString._digits
-        if base > len(digits):
-            return None
+    def apply_n_b_length(self, n, b, length, evaluation):
+        'IntegerDigits[n_Integer, b_Integer, length_Integer]'
+        base = b.get_int_value()
+        if base < 2:
+            return  # error
+        digits = [Integer(d) for d in reversed(_reverse_digits(n.get_int_value(), base))]
+        pad_length = length.get_int_value() - len(digits)
+        if pad_length <= 0:
+            return Expression('List', *digits[-pad_length:])
+        else:
+            zero = Integer(0)
+            return Expression('List', *([zero for _ in range(pad_length)] + digits))
 
-        def rebase(x):
-            while x > 0:
-                yield x % base
-                x //= base
 
-        x = n.get_int_value()
-        remainders = list(rebase(x))
-        if len(remainders) == 0:
-            remainders = [0]
+class DigitCount(Builtin):
+    rules = {
+        'DigitCount[n_Integer]': 'DigitCount[n, 10]'
+    }
 
-        return String(''.join([digits[r] for r in reversed(remainders)]))
+    def apply_n_b_d(self, n, b, d, evaluation):
+        'DigitCount[n_Integer, b_Integer, d_Integer]'
+        base = b.get_int_value()
+        if base < 2:
+            return  # error
+        return Integer(sum(1 for digit in _reverse_digits(n.get_int_value(), base) if digit == d))
 
+    def apply_n_b(self, n, b, evaluation):
+        'DigitCount[n_Integer, b_Integer]'
+        base = b.get_int_value()
+        if base < 2:
+            return  # error
+        occurence_count = [0] * base
+        for digit in _reverse_digits(n.get_int_value(), base):
+            occurence_count[digit] += 1
+        return Expression('List', *occurence_count)
