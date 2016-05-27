78dda284d24bd7c5b426e3c652a0568ed48ed7b9
Bernhard Liebl <Bernhard.Liebl@gmx.org>
2016-05-27 15:45:48 +0200

    a bit of cleanup, docs, test cases

--- mathics/builtin/strings.py
+++ mathics/builtin/strings.py
@@ -1420,33 +1420,10 @@ class StringDrop(Builtin):
         return evaluation.message('StringDrop', 'mseqs')
 
 
-def _one_based(l):
-    return ((i + 1, x) for i, x in l)
-
-
-def _prev_curr(l):
-    prev = None
-    for curr in l:
-        yield prev, curr
-        prev = curr
-
-
 class _StringDistance(Builtin):
     options = {
         'IgnoreCase': 'False'
     }
-
-    def _distance(self, s1, s2, same):
-        if len(s1) < len(s2):
-            s1, s2 = s2, s1
-
-        if len(s2) == 0:
-            return len(s1)
-
-        if len(s1) == len(s2) and all(same(c1, c2) for c1, c2 in zip(s1, s2)):
-            return 0
-
-        return self._compute_distance(s1, s2, same)
 
     def apply(self, a, b, evaluation, options):
         '%(name)s[a_, b_, OptionsPattern[%(name)s]]'
@@ -1469,7 +1446,10 @@ class _StringDistance(Builtin):
 
 # Levenshtein's algorithm is defined by the following construction:
 # (adapted from https://de.wikipedia.org/wiki/Levenshtein-Distanz)
-
+#
+# given two strings s1, s2, we build a matrix D sized (len(s1) + 1,
+# len(s2) + 1) and fill it using the following rules:
+#
 # (1) D(0, 0) = 0
 # (2) D(i, 0) = i, 1 <= i <= len(s1)
 # (3) D(0, j) = j, 1 <= j <= len(s2)
@@ -1478,16 +1458,37 @@ class _StringDistance(Builtin):
 #     D(i - 1, j - 1) + 1 (substitution)
 #     D(i, j - 1) + 1     (insertion)
 #     D(i - 1, j) + 1     (deletion)
-
+#
+# The computed distance will be in D(len(s1) + 1, len(s2) + 1).
+#
 # note: double brackets indicate 1-based indices below, e.g. s1[[1]]
 
-def _levenshtein_row(c1, s2, i, d_prev, same):  # compute D(i, ...), d_prev = D(i - 1, ...)
+def _one_based(l):  # makes an enumerated generator 1-based
+    return ((i + 1, x) for i, x in l)
+
+
+def _prev_curr(l):  # yields pairs of (x[i - 1], x[i]) for i in 1, 2, ...
+    prev = None
+    for curr in l:
+        yield prev, curr
+        prev = curr
+
+
+def _levenshtein_d0(s2):  # compute D(0, ...)
+    return list(range(len(s2) + 1))  # see (1), (3)
+
+
+def _levenshtein_di(c1, s2, i, d_prev, same, cost):  # compute one new row
+    # given c1 = s1[i], s2, i, d_prev = D(i - 1, ...), compute D(i, ...)
+
     yield i  # start with D(i, 0) = i, see (2)
-    d_curr_prev_j = i  # models D(i, j - 1)
+    d_curr_prev_j = i  # d_curr_prev_j stores D(i, j - 1)
 
     for j, c2 in _one_based(enumerate(s2)):  # c2 = s2[[j]]
+        cond = (0 if same(c1, c2) else cost)
+
         d_curr_j = min(  # see (4)
-            d_prev[j - 1] + (0 if same(c1, c2) else 1),  # D(i - 1, j - 1) + cond; substitution
+            d_prev[j - 1] + cond,  # D(i - 1, j - 1) + cond; substitution
             d_curr_prev_j + 1,  # D(i, j - 1) + 1; insertion
             d_prev[j] + 1)  # D(i - 1, j) + 1; deletion
 
@@ -1495,14 +1496,10 @@ def _levenshtein_row(c1, s2, i, d_prev, same):  # compute D(i, ...), d_prev = D(
         d_curr_prev_j = d_curr_j
 
 
-def _levenshtein_row_zero(s2):  # compute D(0, ...)
-    return list(range(len(s2) + 1))  # see (1), (3)
-
-
 def _levenshtein(s1, s2, same):
-    d_prev = _levenshtein_row_zero(s2)
+    d_prev = _levenshtein_d0(s2)
     for i, c1 in _one_based(enumerate(s1)):  # c1 = s1[[i]]
-        d_prev = list(_levenshtein_row(c1, s2, i, d_prev, same))
+        d_prev = list(_levenshtein_di(c1, s2, i, d_prev, same, 1))
     return d_prev[-1]
 
 
@@ -1512,33 +1509,110 @@ def _damerau_levenshtein(s1, s2, same):
     #
     # if i > 1 and j > 1 and a[i] == b[j - 1] and a[i - 1] == b[j] then
     #     D(i, j) = minimum(D(i, j), D(i - 2, j - 2) + transposition_cost)
-    #
-    # note: double brackets indicate 1-based indices below, e.g. s1[[1]]
 
-    def row(d_prev_prev, d_prev, i, prev_c1, c1):  # compute D(i, ...) with:
-        # d_prev = D(i - 1), d_prev_prev = D(i - 2), prev_c1 = s1[[i - 1]], c1 = s1[[i]]
-        for j, d_curr_j in enumerate(_levenshtein_row(c1, s2, i, d_prev, same)):
+    def row(d_prev_prev, d_prev, i, prev_c1, c1, cost):
+        # given c1 = s1[i], d_prev_prev = D(i - 2), d_prev = D(i - 1),
+        # prev_c1 = s1[[i - 1]], c1 = s1[[i]], compute D(i, ...)
+        for j, d_curr_j in enumerate(_levenshtein_di(c1, s2, i, d_prev, same, cost)):
             if i > 1 and j > 1:
-                if same(c1, s2[j - 2]) and same(prev_c1, s2[j - 1]):
+                if same(c1, s2[j - 2]) and same(prev_c1, s2[j - 1]):  # transposition?
                     # i.e. if s1[[i]] = s2[[j-1]] and s1[[i-1]] = s2[[j]]
-                    d_curr_j = min(d_curr_j, d_prev_prev[j - 2] + 1)  # transposition cost
+                    d_curr_j = min(d_curr_j, d_prev_prev[j - 2] + cost)
             yield d_curr_j
 
     d_prev_prev = None
-    d_prev = _levenshtein_row_zero(s2)
+    d_prev = _levenshtein_d0(s2)
     for i, (prev_c1, c1) in _one_based(enumerate(_prev_curr(s1))):
-        d_curr = list(row(d_prev_prev, d_prev, i, prev_c1, c1))
+        d_curr = list(row(d_prev_prev, d_prev, i, prev_c1, c1, 1))
         d_prev_prev = d_prev
         d_prev = d_curr
 
     return d_prev[-1]
 
 
+def _levenshtein_like_or_border_cases(s1, s2, same, compute):
+    if len(s1) == len(s2) and all(same(c1, c2) for c1, c2 in zip(s1, s2)):
+        return 0
+
+    if len(s1) < len(s2):
+        s1, s2 = s2, s1
+
+    if len(s2) == 0:
+        return len(s1)
+
+    return compute(s1, s2, same)
+
+
 class EditDistance(_StringDistance):
-    def _compute_distance(self, s1, s2, same):
-        return _levenshtein(s1, s2, same)
+    """
+    <dl>
+    <dt>'EditDistance[$a$, $b$]'
+        <dd>returns the Levenshtein distance of $a$ and $b$, which is defined as the minimum number of
+        insertions, deletions and substitutions on the constituents of $a$ and $b$ needed to transform
+        one into the other.
+    </dl>
+
+    >> EditDistance["kitten", "kitchen"]
+     = 2
+
+    >> EditDistance["abc", "ac"]
+     = 1
+
+    >> EditDistance["abc", "acb"]
+     = 2
+
+    >> EditDistance["azbc", "abxyc"]
+     = 3
+
+    The IgnoreCase option makes EditDistance ignore the case of letters:
+    >> EditDistance["time", "Thyme"]
+     = 3
+
+    >> EditDistance["time", "Thyme", IgnoreCase -> True]
+     = 2
+
+    EditDistance also works on lists:
+    >> EditDistance[{1, E, 2, Pi}, {1, E, Pi, 2}]
+     = 2
+    """
+
+    def _distance(self, s1, s2, same):
+        return _levenshtein_like_or_border_cases(s1, s2, same, _levenshtein)
 
 
 class DamerauLevenshteinDistance(_StringDistance):
-    def _compute_distance(self, s1, s2, same):
-        return _damerau_levenshtein(s1, s2, same)
+    """
+    <dl>
+    <dt>'DamerauLevenshteinDistance[$a$, $b$]'
+        <dd>returns the Damerau-Levenshtein distance of $a$ and $b$, which is defined as the minimum number of
+        transpositions, insertions, deletions and substitutions needed to transform one into the other.
+        In contrast to EditDistance, DamerauLevenshteinDistance counts transposition of adjacent items (e.g.
+        "ab" into "ba") as one operation of change.
+    </dl>
+
+    >> DamerauLevenshteinDistance["kitten", "kitchen"]
+     = 2
+
+    >> DamerauLevenshteinDistance["abc", "ac"]
+     = 1
+
+    >> DamerauLevenshteinDistance["abc", "acb"]
+     = 1
+
+    >> DamerauLevenshteinDistance["azbc", "abxyc"]
+     = 3
+
+    The IgnoreCase option makes DamerauLevenshteinDistance ignore the case of letters:
+    >> DamerauLevenshteinDistance["time", "Thyme"]
+     = 3
+
+    >> DamerauLevenshteinDistance["time", "Thyme", IgnoreCase -> True]
+     = 2
+
+    DamerauLevenshteinDistance also works on lists:
+    >> DamerauLevenshteinDistance[{1, E, 2, Pi}, {1, E, Pi, 2}]
+     = 1
+    """
+
+    def _distance(self, s1, s2, same):
+        return _levenshtein_like_or_border_cases(s1, s2, same, _damerau_levenshtein)
