074514beecc36720d87bcf24092cbd5aebe25c2a
Angus Griffith <16sn6uv@gmail.com>
2016-07-09 15:03:49 +0200

    tidy Expand docs

--- mathics/builtin/algebra.py
+++ mathics/builtin/algebra.py
@@ -467,15 +467,16 @@ class ExpandAll(_Expand):
     >> ExpandAll[(a + b) ^ 2 / (c + d)^2]
      = a ^ 2 / (c ^ 2 + 2 c d + d ^ 2) + 2 a b / (c ^ 2 + 2 c d + d ^ 2) + b ^ 2 / (c ^ 2 + 2 c d + d ^ 2)
 
-    ExpandAll descends into sub expressions
+    'ExpandAll' descends into sub expressions
     >> ExpandAll[(a + Sin[x (1 + y)])^2]
-     = a ^ 2 + 2 a Sin[x + x y] + Sin[x + x y] ^ 2
+     = 2 a Sin[x + x y] + a ^ 2 + Sin[x + x y] ^ 2
 
-    ExpandAll also expands heads
+    'ExpandAll' also expands heads
     >> ExpandAll[((1 + x)(1 + y))[x]]
      = (1 + x + y + x y)[x]
 
-    #> ExpandAll[(1 + a) ^ 6 / (x + y)^3, Modulus -> 3]
+    'ExpandAll' can also work in finite fields
+    >> ExpandAll[(1 + a) ^ 6 / (x + y)^3, Modulus -> 3]
      = (1 + 2 a ^ 3 + a ^ 6) / (x ^ 3 + y ^ 3)
     """
 
