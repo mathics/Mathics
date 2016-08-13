9313c18fdce8a0da9d6fa707177f21ec2d281ca8
Bernhard Liebl <Bernhard.Liebl@gmx.org>
2016-08-13 07:17:51 +0200

    undo change in files.py; to be addressed in separate PR

--- mathics/builtin/files.py
+++ mathics/builtin/files.py
@@ -795,7 +795,8 @@ class Write(Builtin):
         expr = expr.get_sequence()
         expr = Expression('Row', Expression('List', *expr))
 
-        text = evaluation.format_output(from_python(expr), 'text')
+        evaluation.format = 'text'
+        text = evaluation.format_output(from_python(expr))
         stream.write(six.text_type(text) + '\n')
         return Symbol('Null')
 
