5800f31c7414a9efda653d25d4d70e239113eb02
Bernhard Liebl <Bernhard.Liebl@gmx.org>
2016-09-01 08:19:38 +0200

    palettable is now a default install; fixes bug and cosmetics with box calculation in BarChart

--- setup.py
+++ setup.py
@@ -35,9 +35,9 @@ if sys.version_info < (3, 6):
     sys.exit(-1)
 
 # stores __version__ in the current namespace
-exec(compile(open('mathics/version.py').read(), 'mathics/version.py', 'exec'))
+exec(compile(open("mathics/version.py").read(), "mathics/version.py", "exec"))
 
-is_PyPy = (platform.python_implementation() == 'PyPy')
+is_PyPy = platform.python_implementation() == "PyPy"
 
 INSTALL_REQUIRES = []
 DEPENDENCY_LINKS = []
@@ -51,24 +51,33 @@ except ImportError:
     CMDCLASS = {}
 else:
     EXTENSIONS = {
-        'core': ['expression', 'numbers', 'rules', 'pattern'],
-        'builtin': ['arithmetic', 'numeric', 'patterns', 'graphics']
+        "core": ["expression", "numbers", "rules", "pattern"],
+        "builtin": ["arithmetic", "numeric", "patterns", "graphics"],
     }
     EXTENSIONS = [
-        Extension('mathics.%s.%s' % (parent, module),
-                  ['mathics/%s/%s.py' % (parent, module)])
-        for parent, modules in EXTENSIONS.items() for module in modules]
-    CMDCLASS = {'build_ext': build_ext}
-    INSTALL_REQUIRES += ['cython>=0.15.1']
+        Extension(
+            "mathics.%s.%s" % (parent, module), ["mathics/%s/%s.py" % (parent, module)]
+        )
+        for parent, modules in EXTENSIONS.items()
+        for module in modules
+    ]
+    CMDCLASS = {"build_ext": build_ext}
+    INSTALL_REQUIRES += ["cython>=0.15.1"]
 
 # General Requirements
-INSTALL_REQUIRES += ['sympy>=1.6, < 1.7', 'django >= 1.8, < 1.12',
-                     'mpmath>=1.1.0', 'python-dateutil', 'colorama']
+INSTALL_REQUIRES += [
+    "sympy>=1.6, < 1.7",
+    "django >= 1.8, < 1.12",
+    "mpmath>=1.1.0",
+    "python-dateutil",
+    "colorama",
+    "palettable",
+]
 
 
-def subdirs(root, file='*.*', depth=10):
+def subdirs(root, file="*.*", depth=10):
     for k in range(depth):
-        yield root + '*/' * k + file
+        yield root + "*/" * k + file
 
 
 class initialize(Command):
@@ -86,23 +95,28 @@ class initialize(Command):
     def initialize_options(self):  # distutils wants this
         pass
 
-    def finalize_options(self):    # this too
+    def finalize_options(self):  # this too
         pass
 
     def run(self):
         import os
         import subprocess
-        settings = {}
-        exec(compile(open('mathics/settings.py').read(), 'mathics/settings.py', 'exec'), settings)
 
-        database_file = settings['DATABASES']['default']['NAME']
-        print("Creating data directory %s" % settings['DATA_DIR'])
-        if not os.path.exists(settings['DATA_DIR']):
-            os.makedirs(settings['DATA_DIR'])
+        settings = {}
+        exec(
+            compile(open("mathics/settings.py").read(), "mathics/settings.py", "exec"),
+            settings,
+        )
+
+        database_file = settings["DATABASES"]["default"]["NAME"]
+        print("Creating data directory %s" % settings["DATA_DIR"])
+        if not os.path.exists(settings["DATA_DIR"]):
+            os.makedirs(settings["DATA_DIR"])
         print("Creating database %s" % database_file)
         try:
             subprocess.check_call(
-                [sys.executable, 'mathics/manage.py', 'migrate', '--noinput'])
+                [sys.executable, "mathics/manage.py", "migrate", "--noinput"]
+            )
             print("")
             print("Database created successfully.")
         except subprocess.CalledProcessError:
@@ -125,76 +139,88 @@ class test(Command):
     def initialize_options(self):  # distutils wants this
         pass
 
-    def finalize_options(self):    # this too
+    def finalize_options(self):  # this too
         pass
 
     def run(self):
         import unittest
+
         test_loader = unittest.defaultTestLoader
         test_runner = unittest.TextTestRunner(verbosity=3)
-        test_suite = test_loader.discover('test/')
+        test_suite = test_loader.discover("test/")
         test_result = test_runner.run(test_suite)
 
         if not test_result.wasSuccessful():
             sys.exit(1)
 
 
-CMDCLASS['initialize'] = initialize
-CMDCLASS['test'] = test
+CMDCLASS["initialize"] = initialize
+CMDCLASS["test"] = test
 
-mathjax_files = list(subdirs('media/js/mathjax/'))
+mathjax_files = list(subdirs("media/js/mathjax/"))
 
 setup(
     name="Mathics",
     cmdclass=CMDCLASS,
     ext_modules=EXTENSIONS,
     version=__version__,
-
     packages=[
-        'mathics',
-        'mathics.algorithm',
-        'mathics.core',
-        'mathics.core.parser',
-        'mathics.builtin', 'mathics.builtin.pymimesniffer', 'mathics.builtin.numpy_utils',
-        'mathics.builtin.pympler', 'mathics.builtin.compile',
-        'mathics.doc',
-        'mathics.web', 'mathics.web.templatetags', 'mathics.web.migrations',
-        'pymathics',
-        'pymathics.testpymathicsmodule',
-        'pymathics.natlang',
+        "mathics",
+        "mathics.algorithm",
+        "mathics.core",
+        "mathics.core.parser",
+        "mathics.builtin",
+        "mathics.builtin.pymimesniffer",
+        "mathics.builtin.numpy_utils",
+        "mathics.builtin.pympler",
+        "mathics.builtin.compile",
+        "mathics.doc",
+        "mathics.web",
+        "mathics.web.templatetags",
+        "mathics.web.migrations",
+        "pymathics",
+        "pymathics.testpymathicsmodule",
+        "pymathics.natlang",
     ],
-
     install_requires=INSTALL_REQUIRES,
     dependency_links=DEPENDENCY_LINKS,
-
     package_data={
-        'mathics': [
-            'data/*.csv', 'data/ExampleData/*',
-            'autoload/formats/*/Import.m', 'autoload/formats/*/Export.m',
-            'packages/*/*.m', 'packages/*/Kernel/init.m'],
-        'mathics.doc': ['documentation/*.mdoc', 'xml/data'],
-        'mathics.web': [
-            'media/css/*.css', 'media/img/*.*', 'media/fonts/*',
-            'media/img/favicons/*',
-            'media/js/innerdom/*.js', 'media/js/prototype/*.js',
-            'media/js/scriptaculous/*.js', 'media/js/three/Three.js',
-            'media/js/three/Detector.js', 'media/js/*.js', 'templates/*.html',
-            'templates/doc/*.html'] + mathjax_files,
-        'mathics.builtin.pymimesniffer': ['mimetypes.xml'],
-        'pymathics': ['doc/documentation/*.mdoc', 'doc/xml/data'],
+        "mathics": [
+            "data/*.csv",
+            "data/ExampleData/*",
+            "autoload/formats/*/Import.m",
+            "autoload/formats/*/Export.m",
+            "packages/*/*.m",
+            "packages/*/Kernel/init.m",
+        ],
+        "mathics.doc": ["documentation/*.mdoc", "xml/data"],
+        "mathics.web": [
+            "media/css/*.css",
+            "media/img/*.*",
+            "media/fonts/*",
+            "media/img/favicons/*",
+            "media/js/innerdom/*.js",
+            "media/js/prototype/*.js",
+            "media/js/scriptaculous/*.js",
+            "media/js/three/Three.js",
+            "media/js/three/Detector.js",
+            "media/js/*.js",
+            "templates/*.html",
+            "templates/doc/*.html",
+        ]
+        + mathjax_files,
+        "mathics.builtin.pymimesniffer": ["mimetypes.xml"],
+        "pymathics": ["doc/documentation/*.mdoc", "doc/xml/data"],
     },
-
     entry_points={
-        'console_scripts': [
-            'mathics = mathics.main:main',
-            'mathicsserver = mathics.server:main',
-            'mathicsscript = mathics.script:main',
+        "console_scripts": [
+            "mathics = mathics.main:main",
+            "mathicsserver = mathics.server:main",
+            "mathicsscript = mathics.script:main",
         ],
     },
-
     # don't pack Mathics in egg because of media files, etc.
     zip_safe=False,
-
     # metadata for upload to PyPI
     author="Angus Griffith",
     author_email="mathics@angusgriffith.com",
@@ -202,21 +228,21 @@ setup(
     license="GPL",
     url="https://mathics.github.io/",
     download_url="https://github.com/mathics/Mathics/tarball/v0.9",
-    keywords=['Mathematica', 'Wolfram', 'Interpreter', 'Shell', 'Math', 'CAS'],
+    keywords=["Mathematica", "Wolfram", "Interpreter", "Shell", "Math", "CAS"],
     classifiers=[
-        'Intended Audience :: Developers',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Topic :: Scientific/Engineering',
-        'Topic :: Scientific/Engineering :: Mathematics',
-        'Topic :: Scientific/Engineering :: Physics',
-        'Topic :: Software Development :: Interpreters',
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "Topic :: Scientific/Engineering :: Physics",
+        "Topic :: Software Development :: Interpreters",
     ],
     # TODO: could also include long_description, download_url,
 )
