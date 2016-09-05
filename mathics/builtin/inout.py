cf4c940e9f66f8d21ad759658b0eb66ff88ff717
Angus Griffith <16sn6uv@gmail.com>
2016-09-05 12:41:33 +1000

    zero padding integer x0.0 case

--- mathics/builtin/inout.py
+++ mathics/builtin/inout.py
@@ -139,10 +139,10 @@ def real_to_s_exp(expr, n):
             exp = int(exp)
             if len(s) > 1 and s[1] == '.':
                 # str(float) doesn't always include '.' if 'e' is present.
-                s = s[0] + s[2:]
+                s = s[0] + s[2:].rstrip('0')
         else:
             exp = s.index('.') - 1
-            s = s[:exp + 1] + s[exp + 2:]
+            s = s[:exp + 1] + s[exp + 2:].rstrip('0')
 
             # consume leading '0's.
             i = 0
@@ -150,7 +150,6 @@ def real_to_s_exp(expr, n):
                 i += 1
                 exp -= 1
             s = s[i:]
-        s = s.rstrip('0')
 
         # add trailing zeros for precision reals
         if n is not None and not expr.is_machine_precision() and len(s) < n:
@@ -2170,6 +2169,12 @@ class NumberForm(_NumberForm):
     ## 1-arg, Option case
     #> NumberForm[34, ExponentFunction->(Null&)]
      = 34
+
+    ## zero padding integer x0.0 case
+    #> NumberForm[50.0, {5, 1}]
+     = 50.0
+    #> NumberForm[50, {5, 1}]
+     = 50.0
     '''
 
     options = {
