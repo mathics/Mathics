40198ecbc0287c6ac42efe1c2d33f8aa703f1f85
Angus Griffith <16sn6uv@gmail.com>
2016-09-16 00:08:53 +1000

    more Power tests

--- test/test_compile.py
+++ test/test_compile.py
@@ -85,11 +85,24 @@ class ArithmeticTest(CompileTest):
     def test_pow_real(self):
         self._test_binary_math('Power', mpmath.power)
 
+    @unittest.expectedFailure
     def test_pow_int(self):
+        expr = Expression('Power', Symbol('x'), Symbol('y'))
+        args = [CompileArg('System`x', int_type), CompileArg('System`y', int_type)]
+        cfunc = _compile(expr, args)
+        self.assertIs(cfunc(4, 9), 4 ** 9)
+
+    def test_pow_real_int(self):
         expr = Expression('Power', MachineReal(2.5), Symbol('x'))
         args = [CompileArg('System`x', int_type)]
         cfunc = _compile(expr, args)
-        self.assertEquals(cfunc(4), 2.5 ** 4)
+        self.assertIs(cfunc(4), 2.5 ** 4)
+
+    def test_pow_int_real(self):
+        expr = Expression('Power', Symbol('x'), MachineReal(5.5))
+        args = [CompileArg('System`x', int_type)]
+        cfunc = _compile(expr, args)
+        self.assertIs(cfunc(8), 8 ** 5.5)
 
     def test_pow_2(self):
         # 2 ^ x
