74ccec5dea2b9d8089cbce7d8f8d81bb44c6fca2
Bernhard Liebl <Bernhard.Liebl@gmx.org>
2016-09-22 06:12:35 +0200

    fixes test case

--- mathics/builtin/xmlformat.py
+++ mathics/builtin/xmlformat.py
@@ -258,8 +258,8 @@ class XMLObjectImport(Builtin):
     >> Part[Import["ExampleData/InventionNo1.xml", "XMLObject"], 2, 3, 1]
      = XMLElement[identification, {}, {XMLElement[encoding, {}, {XMLElement[software, {}, {MuseScore 1.2}], XMLElement[encoding-date, {}, {2012-09-12}]}]}]
 
-    >> Import["ExampleData/Namespaces.xml"]
-     = XMLObject[Document][{XMLObject[Declaration][Version -> 1.0, Encoding -> UTF-8]}, XMLElement[book, {{http://www.w3.org/2000/xmlns/, xmlns} -> urn:loc.gov:books}, {XMLElement[title, {}, {Cheaper by the Dozen}], XMLElement[{urn:ISBN:0-395-36341-6, number}, {}, {1568491379}], XMLElement[notes, {}, {XMLElement[p, {{http://www.w3.org/2000/xmlns/, xmlns} -> http://www.w3.org/1999/xhtml}, {This is a, XMLElement[i, {}, {funny, book!}]}]}]}]]
+    >> Part[Import["ExampleData/Namespaces.xml"], 2]
+     = XMLElement[book, {{http://www.w3.org/2000/xmlns/, xmlns} -> urn:loc.gov:books}, {XMLElement[title, {}, {Cheaper by the Dozen}], XMLElement[{urn:ISBN:0-395-36341-6, number}, {}, {1568491379}], XMLElement[notes, {}, {XMLElement[p, {{http://www.w3.org/2000/xmlns/, xmlns} -> http://www.w3.org/1999/xhtml}, {This is a, XMLElement[i, {}, {funny, book!}]}]}]}]
     """
 
     context = 'XML`'
