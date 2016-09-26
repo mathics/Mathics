ff7ce2b64e1e0ea3b96852e82b9d698498e78009
mmatera <matera@fisica.unlp.edu.ar>
2016-09-26 12:36:01 -0300

    Catch Lookup error

--- mathics/builtin/natlang.py
+++ mathics/builtin/natlang.py
@@ -119,6 +119,9 @@ try:
         'Adjective': [nltk.corpus.wordnet.ADJ, nltk.corpus.wordnet.ADJ_SAT],
         'Adverb': [nltk.corpus.wordnet.ADV],
     }
+except LookupError:
+    _wordnet_pos_to_type = {}
+    _wordnet_type_to_pos = {}
 except ImportError:
     _wordnet_pos_to_type = {}
     _wordnet_type_to_pos = {}
