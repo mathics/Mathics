7e89f7a7366855c228a27dff3b24982f4b3af508
Bernhard Liebl <Bernhard.Liebl@gmx.org>
2016-09-27 19:16:49 +0200

    fixes typo bug

--- mathics/core/expression.py
+++ mathics/core/expression.py
@@ -1007,7 +1007,7 @@ class Expression(BaseExpression):
         for index, leaf in enumerate(new.leaves):
             if leaf.unevaluated:
                 new.leaves[index] = Expression('Unevaluated', leaf)
-                new.sym = None
+                new._symbols = None
 
         new.unformatted = self.unformatted
         new.last_evaluated = evaluation.definitions.now
