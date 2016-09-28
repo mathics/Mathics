5d059a702d6f6e324b24bde7ac1962a0e64c682c
Bernhard Liebl <Bernhard.Liebl@gmx.org>
2016-09-28 18:21:36 +0200

    fixes test cases, adds MakeBoxes rule

--- mathics/builtin/linalg.py
+++ mathics/builtin/linalg.py
@@ -436,6 +436,13 @@ class FittedModel(Builtin):
     rules = {
         'FittedModel[x_List][s_String]': 's /. x',
         'FittedModel[x_List][y_]': '("Function" /. x)[y]',
+
+        'MakeBoxes[FittedModel[x_List], f_]':
+            '''
+            RowBox[{"FittedModel[",
+                Replace[Temporary["BestFit" /. x, f], Temporary -> MakeBoxes, 1, Heads -> True],
+                "]"}]
+            ''',
     }
 
 
@@ -470,7 +477,7 @@ class LinearModelFit(Builtin):
         <dd>returns the design matrix.
     </dl>
 
-    >> m = LinearModelFit[{{2, 1}, {3, 4}, {5, 3}, {7, 6}}, x, x]
+    >> m = LinearModelFit[{{2, 1}, {3, 4}, {5, 3}, {7, 6}}, x, x];
     >> m["BasisFunctions"]
      = {1, x}
 
@@ -492,14 +499,14 @@ class LinearModelFit(Builtin):
     >> m["FitResiduals"]
      = {-0.745763, 1.47458, -1.08475, 0.355932}
 
-    >> m = LinearModelFit[{{2, 2, 1}, {3, 2, 4}, {5, 6, 3}, {7, 9, 6}}, {Sin[x], Cos[y]}, {x, y}]
+    >> m = LinearModelFit[{{2, 2, 1}, {3, 2, 4}, {5, 6, 3}, {7, 9, 6}}, {Sin[x], Cos[y]}, {x, y}];
     >> m["BasisFunctions"]
      = {1, Sin[x], Cos[y]}
 
     >> m["Function"]
      = 3.33077 - 5.65221 Cos[#2] - 5.01042 Sin[#1]&
 
-    >> m = LinearModelFit[{{{1, 4}, {1, 5}, {1, 7}}, {1, 2, 3}}]
+    >> m = LinearModelFit[{{{1, 4}, {1, 5}, {1, 7}}, {1, 2, 3}}];
     >> m["BasisFunctions"]
      = {#1, #2}
 
