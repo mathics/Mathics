55f112e7b1077e94af45fc972ba79398736f1ae5
Bernhard Liebl <Bernhard.Liebl@gmx.org>
2016-10-19 20:58:10 +0200

    fixes version numbers for pillow, scikit-image

--- travis.py
+++ travis.py
@@ -12,11 +12,11 @@ modules = (
     "lxml",
     "matplotlib",
     "networkx",
-    "PIL",
+    "pillow",
     "wordcloud",
     "pycor",
     "scipy",
-    "skimage",
+    "scikit-image",
 )
 
 print('installed modules:')
