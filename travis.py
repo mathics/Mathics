e4f1b06043eb7c2c35c6014f1e5756e3a67b6ec1
Bernhard Liebl <Bernhard.Liebl@gmx.org>
2016-10-19 22:15:50 +0200

    added spacy, now sorted

--- travis.py
+++ travis.py
@@ -5,6 +5,7 @@ modules = (
     "ipywidgets",
     "ipykernel",
     "IPython",
+    "spacy",
     "nltk",
     "langid",
     "pycountry",
@@ -21,7 +22,7 @@ modules = (
 
 print('installed modules:')
 
-for module in modules:
+for module in sorted(modules):
     try:
         print('    ' + str(pkg_resources.get_distribution(module)))
     except pkg_resources.DistributionNotFound:
