8c947c14cc1d492da1c09e692e211f463061d667
mauricio matera <matera@fisica.unlp.edu.ar>
2016-11-07 10:41:56 -0300

    clean definition inspections

--- mathics/core/definitions.py
+++ mathics/core/definitions.py
@@ -687,6 +687,6 @@ class Definition(object):
         return False
 
     def __repr__(self):
-        s = '<Definition: name: {}, downvalues: {}, formats: {}, attributes: {}  dict: {} >'.format(
-            self.name, self.downvalues, self.formatvalues, self.attributes, self.__dict__)
+        s = '<Definition: name: {}, downvalues: {}, formats: {}, attributes: {} >'.format(
+            self.name, self.downvalues, self.formatvalues, self.attributes)
         return s.encode('unicode_escape')
