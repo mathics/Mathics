a9822de55a40414c83a352b915f45f228e1e9a04
mauricio matera <matera@fisica.unlp.edu.ar>
2016-11-28 17:03:25 -0300

    fixing indices

--- mathics/settings.py
+++ mathics/settings.py
@@ -119,7 +119,7 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'mathics.urls'
 
-default_pymathics_modules = ["pymathics",]
+default_pymathics_modules = ["pymathics.natlang","pymathics.testpymathicsmodule"]
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or
