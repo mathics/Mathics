9741e8cfa3cb71dc2f0c72dc8115090fa3643d26
larion_dev <notarge10@gmail.com>
2017-04-26 16:50:19 +0700

    Add ContainsOnly[]

--- mathics/builtin/lists.py
+++ mathics/builtin/lists.py
@@ -4818,3 +4818,107 @@ class Permutations(Builtin):
         return Expression('List', *[Expression('List', *p)
                                     for r in rs
                                     for p in permutations(l.leaves, r)])
+
+class ContainsOnly(Builtin):
+    """
+    <dl>
+    <dt>'ContainsOnly[$list1$, $list2$]'
+        <dd>yields True if $list1$ contains only elements that appear in $list2$.
+    </dl>
+
+    >> ContainsOnly[{b, a, a}, {a, b, c}]
+     = True
+
+    The first list contains elements not present in the second list:
+    >> ContainsOnly[{b, a, d}, {a, b, c}]
+     = False
+
+    >> ContainsOnly[{}, {a, b, c}]
+     = True
+
+    #> ContainsOnly[1, {1, 2, 3}]
+     : List or association expected instead of 1.
+     = ContainsOnly[1, {1, 2, 3}]
+
+    #> ContainsOnly[{1, 2, 3}, 4]
+     : List or association expected instead of 4.
+     = ContainsOnly[{1, 2, 3}, 4]
+
+    Use Equal as the comparison function to have numerical tolerance:
+    >> ContainsOnly[{a, 1.0}, {1, a, b}, {SameTest -> Equal}]
+     = True
+     
+    #> ContainsOnly[{c, a}, {a, b, c}, IgnoreCase -> True]
+     : Unknown option IgnoreCase for ContainsOnly.
+     = True
+    
+    #> ContainsOnly[{a, 1.0}, {1, a, b}, {IgnoreCase -> True, SameTest -> Equal}]
+     : Unknown option IgnoreCase for ContainsOnly.
+     = True
+
+    #> ContainsOnly[Pi, "E", {IgnoreCase -> True, SameTest -> Equal}]
+     : List or association expected instead of E.
+     : Unknown option IgnoreCase in ContainsOnly[Pi, E, {IgnoreCase -> True, SameTest -> Equal}].
+     = ContainsOnly[Pi, E, {IgnoreCase -> True, SameTest -> Equal}]
+
+    #> ContainsOnly["Pi", E, {IgnoreCase -> True, SameTest -> Equal}]
+     : List or association expected instead of Pi.
+     : Unknown option IgnoreCase in ContainsOnly[Pi, E, {IgnoreCase -> True, SameTest -> Equal}].
+     = ContainsOnly[Pi, E, {IgnoreCase -> True, SameTest -> Equal}]
+
+    #> ContainsOnly[Pi, E, {IgnoreCase -> True, SameTest -> Equal}]
+     : Unknown option IgnoreCase in ContainsOnly[Pi, E, {IgnoreCase -> True, SameTest -> Equal}].
+     = ContainsOnly[Pi, E, {IgnoreCase -> True, SameTest -> Equal}]
+    """
+
+    attributes = ('ReadProtected',)
+    
+    messages = {
+        'lsa': "List or association expected instead of `1`.",
+        'nodef': "Unknown option `1` for ContainsOnly.",
+        'optx': "Unknown option `1` in `2`.",
+    }
+
+    options = {
+        'SameTest': 'SameQ',
+    }
+
+    def check_options(self, expr, evaluation, options):
+        for key in options:
+            if key != 'System`SameTest':
+                if expr is None:
+                    evaluation.message('ContainsOnly', 'nodef', Symbol(key))
+                else:
+                    return evaluation.message('ContainsOnly', 'optx', Symbol(key), expr)
+        return None
+
+    def apply(self, list1, list2, evaluation, options={}):
+        'ContainsOnly[list1_?ListQ, list2_?ListQ, OptionsPattern[ContainsOnly]]'
+
+        same_test = self.get_option(options, 'SameTest', evaluation)
+
+        def same(a, b):
+            result = Expression(same_test, a, b).evaluate(evaluation)
+            return result.is_true()
+
+        self.check_options(None, evaluation, options)
+        for a in list1.leaves:
+            if not any(same(a, b) for b in list2.leaves):
+                return Symbol('False')
+        return Symbol('True')
+
+    def apply_msg(self, e1, e2, evaluation, options={}):
+        'ContainsOnly[e1_, e2_, OptionsPattern[ContainsOnly]]'
+
+        opts = options_to_rules(options) if len(options) <= 1 else [Expression('List', *options_to_rules(options))]
+        expr = Expression('ContainsOnly', e1, e2, *opts)
+
+        if not isinstance(e1, Symbol) and not e1.has_form('List', None):
+            evaluation.message('ContainsOnly', 'lsa', e1)
+            return self.check_options(expr, evaluation, options)
+
+        if not isinstance(e2, Symbol) and not e2.has_form('List', None):
+            evaluation.message('ContainsOnly', 'lsa', e2)
+            return self.check_options(expr, evaluation, options)
+
+        return self.check_options(expr, evaluation, options)
