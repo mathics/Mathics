b9229cc800d93aa924aa9b54fa069397c95ba828
larion_dev <notarge10@gmail.com>
2017-05-22 16:57:41 +0700

    Add Check

--- mathics/builtin/inout.py
+++ mathics/builtin/inout.py
@@ -1191,6 +1191,129 @@ def check_message(expr):
             return True
     return False
 
+class Check(Builtin):
+    """
+    <dl>
+    <dt>'Check[$expr$, $failexpr$]'
+        <dd>evaluates $expr$, and returns the result, unless messages were generated, in which case it evaluates and $failexpr$ will be returned.
+    <dt>'Check[$expr$, $failexpr$, {s1::t1,s2::t2,…}]'
+        <dd>checks only for the specified messages.
+    </dl>
+    
+    Return err when a message is generated:
+    >> Check[1/0, err]
+     : Infinite expression 1 / 0 encountered.
+     = err
+     
+    #> Check[1^0, err]
+     = 1
+     
+    Check only for specific messages: 
+    >> Check[Sin[0^0], err, Sin::argx]
+     : Indeterminate expression 0 ^ 0 encountered.
+     = Indeterminate
+     
+    >> Check[1/0, err, Power::infy]
+     : Infinite expression 1 / 0 encountered.
+     = err
+     
+    #> Check[1 + 2]
+     : Check called with 1 argument; 2 or more arguments are expected.
+     = Check[1 + 2]
+     
+    #> Check[1 + 2, err, 3 + 1]
+     : Message name 3 + 1 is not of the form symbol::name or symbol::name::language.
+     = Check[1 + 2, err, 3 + 1]
+     
+    #> Check[1 + 2, err, hello]
+     : Message name hello is not of the form symbol::name or symbol::name::language.
+     = Check[1 + 2, err, hello]
+      
+    #> Check[1/0, err, Compile::cpbool]
+     : Infinite expression 1 / 0 encountered.
+     = ComplexInfinity
+    
+    #> Check[{0^0, 1/0}, err]
+     : Indeterminate expression 0 ^ 0 encountered.
+     : Infinite expression 1 / 0 encountered.
+     = err
+    
+    #> Check[0^0/0, err, Power::indet]
+     : Indeterminate expression 0 ^ 0 encountered.
+     : Infinite expression 1 / 0 encountered.
+     = err
+     
+    #> Check[{0^0, 3/0}, err, Power::indet]
+     : Indeterminate expression 0 ^ 0 encountered.
+     : Infinite expression 1 / 0 encountered.
+     = err
+    
+    #> Check[1 + 2, err, {a::b, 2 + 5}]
+     : Message name 2 + 5 is not of the form symbol::name or symbol::name::language.
+     = Check[1 + 2, err, {a::b, 2 + 5}] 
+    
+    #> Off[Power::infy]
+    #> Check[1 / 0, err]
+     = ComplexInfinity
+     
+    #> On[Power::infy]
+    #> Check[1 / 0, err]
+     : Infinite expression 1 / 0 encountered.
+     = err
+    """
+
+    attributes = ('HoldAll',)
+
+    messages = {
+        'argmu': 'Check called with 1 argument; 2 or more arguments are expected.',
+        'name': 'Message name `1` is not of the form symbol::name or symbol::name::language.',
+    }
+    
+    def apply_1_argument(self, expr, evaluation):
+        'Check[expr_]'
+        return evaluation.message('Check', 'argmu')
+    
+    def apply(self, expr, failexpr, params, evaluation):
+        'Check[expr_, failexpr_, params___]'
+        
+        #Todo: To implement the third form of this function , we need to implement the function $MessageGroups first          
+            #<dt>'Check[$expr$, $failexpr$, "name"]'
+               #<dd>checks only for messages in the named message group.
+                 
+        def get_msg_list(exprs):
+            messages = []
+            for expr in exprs:
+                if expr.has_form('List', None):
+                    messages.extend(get_msg_list(expr.leaves))
+                elif check_message(expr):
+                    messages.append(expr)
+                else:
+                    raise Exception(expr)
+            return messages
+   
+        check_messages = set(evaluation.get_quiet_messages())
+        display_fail_expr = False
+
+        params = params.get_sequence()    
+        if len(params) == 0:
+            result = expr.evaluate(evaluation)
+            if(len(evaluation.out)):
+                display_fail_expr = True
+        else:
+            try:
+                msgs = get_msg_list(params)
+                for x in msgs: 
+                    check_messages.add(x)
+            except Exception as inst :
+                evaluation.message('Check', 'name', inst.args[0])
+                return 
+            result = expr.evaluate(evaluation)
+            for out_msg in evaluation.out:
+                pattern = Expression('MessageName', Symbol(out_msg.symbol), String(out_msg.tag))
+                if pattern in check_messages:
+                    display_fail_expr = True
+                    break
+        return failexpr if display_fail_expr is True else result
 
 class Quiet(Builtin):
     """
