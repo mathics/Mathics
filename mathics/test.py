6f4ddcb473fb84936e90e6f2930148a22b20bc54
Pablo Emilio Escobar Gaviria <pablo-escobar@riseup.net>
2020-09-14 20:58:50 -0300

    Added -halt-on-error to the flags passed to xelatex

--- mathics/test.py
+++ mathics/test.py
@@ -309,9 +309,8 @@ def main():
         help="skip the first N tests",
     )
     args = parser.parse_args()
+    generate_output = args.output or args.tex
 
-    if args.tex:
-        write_latex()
     elif args.section:
         test_section(args.section, stop_on_failure=args.stop_on_failure)
     else:
@@ -321,9 +320,12 @@ def main():
             test_section(args.section, stop_on_failure=args.stop_on_failure)
         else:
             start_at = args.skip + 1
-            test_all(quiet=args.quiet, generate_output=args.output,
+            test_all(quiet=args.quiet, generate_output=generate_output,
                      stop_on_failure=args.stop_on_failure,
                      start_at=start_at)
+
+            if args.tex:
+                write_latex()
 
 
 if __name__ == "__main__":
