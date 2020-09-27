9e88fad78755d5504708291d17beb3232403f3e7
rocky <rb@dustyfeet.com>
2020-09-27 05:50:58 -0400

    One more Sum example

--- mathics/web/media/js/inout.js
+++ mathics/web/media/js/inout.js
@@ -193,6 +193,7 @@ function showGallery() {
 	    'N[E, 30] (* 30-digit Numeric approximation of E *)',
 	    '30!',
 	    '30! // N',
+	    'Sum[2 i + 1, {i, 0, 10}] (* Sum of 1st n odd numbers (n+1)**2 *)',
 	    'n = 8; 2 ^ # & /@ Range[0, n] (* Powers of 2 *)',
 	    'Total[%] (* Sum is 2 ^ n - 1 *)',
 	    '(* Symbolic Manipulation *)',
