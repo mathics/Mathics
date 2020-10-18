b5f3e4af0e9b06cc30e801a99cc6028745961765
rocky <rb@dustyfeet.com>
2020-10-17 21:52:06 -0400

    Add pytests for Global System Information section

--- test/test_evaluation.py
+++ test/test_evaluation.py
@@ -13,19 +13,42 @@ def _evaluate(str_expression):
     return expr.evaluate(evaluation)
 
 
-@pytest.mark.parametrize('str_expr,str_expected', [
-    (r'Table[F[x],{x,1,3}]', '{F[1],F[2],F[3]}'),
-    (r'Table[F[x],{x,{1,2,3}}]', '{F[1],F[2],F[3]}'),
-    (r's={1,2,3};Table[F[x],{x,s}]', '{F[1],F[2],F[3]}'),
-    (r's={x,1,3};Table[F[x],s]', '{F[1],F[2],F[3]}'),
-    (r's={x,{1,2,3}};Table[F[x],s]', '{F[1],F[2],F[3]}'),
-])
-def test_evaluation(str_expr, str_expected):
+@pytest.mark.parametrize(
+    "str_expr,str_expected",
+    [
+        # Table tests
+        (r"Table[F[x],{x,1,3}]", "{F[1],F[2],F[3]}"),
+        (r"Table[F[x],{x,{1,2,3}}]", "{F[1],F[2],F[3]}"),
+        (r"s={1,2,3};Table[F[x],{x,s}]", "{F[1],F[2],F[3]}"),
+        (r"s={x,1,3};Table[F[x],s]", "{F[1],F[2],F[3]}"),
+        (r"s={x,{1,2,3}};Table[F[x],s]", "{F[1],F[2],F[3]}"),
+        (r"s={x,{1,2,3}};Table[F[x],s]", "{F[1],F[2],F[3]}"),
+        # Global System Information
+        (r"Abs[$ByteOrdering]", "1"),
+        (r"Head[$CommandLine]", "List"),
+        (r"Head[$Machine]", "String"),
+        (r"Head[$MachineName]", "String"),
+        (r"""Length[Names["System`*"]] > 1024""", "True"),
+        (r"Length[$Packages] >= 5", "True"),
+        (r"Head[$ParentProcessID]", "Integer"),
+        (r"Head[$ProcessID]", "Integer"),
+        (r"Head[$ProcessorType]", "String"),
+        (r"Head[$ScriptCommandLine]", "List"),
+        (r"Head[$SystemID]", "String"),
+        (r"Head[$SystemWordLength]", "Integer"),
+        # This doesn't work if not logged or in some OS's
+        # (r"Head[$UserName]", "String"),
+        (r"Head[$Version]", "String"),
+    ],
+)
+def test_evaluation(str_expr: str, str_expected: str, message=""):
     result = _evaluate(str_expr)
     expected = _evaluate(str_expected)
 
-    assert result == expected
-
+    if message:
+        assert result == expected, message
+    else:
+        assert result == expected
 
 
 def test_exit():
@@ -40,4 +63,3 @@ def test_quit():
         _evaluate("Quit[-37]")
     except SystemExit as e:
         assert e.code == -37
-
