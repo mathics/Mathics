2a2e8c7ef3922c59806d5b3c04500c9ff1469699
mmatera <matera@fisica.unlp.edu.ar>
2020-11-08 18:47:13 -0300

    improving session object

--- mathics/session.py
+++ mathics/session.py
@@ -9,11 +9,17 @@ class MathicsSession:
         self.definitions = Definitions(add_builtin)
         self.evaluation = Evaluation(definitions=self.definitions, catch_interrupt=catch_interrupt)
         self.form = form
+        self.last_result = None
         
-    def evaluate(self, str_expression, timeout = None, form= None):
+    def evaluate(self, str_expression, timeout = None, form=None):
         expr = parse(self.definitions, SingleLineFeeder(str_expression))
         if form is None:
             form = self.form
-        return expr.evaluate(self.evaluation).format(ms.evaluation, form)
+        self.last_result = expr.evaluate(self.evaluation)
+        return self.last_result
 
-
+    def format_result(self, str_expression=None, timeout = None, form=None):
+        self.last_result
+        if form is None:
+            form = self.form
+        return res.do_format(self.evaluation, form)
