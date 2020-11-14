d41fae8ed4e4cd52548d3c52e7f641f831d7a954
rocky <rb@dustyfeet.com>
2020-11-14 12:30:00 -0500

    Get ready for release 1.1.0
    Administrivia:
    * Change PyPI package name to Mathics3
    * wrong package name in make-dist.sh script

--- admin-tools/make-dist.sh
+++ admin-tools/make-dist.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PACKAGE=mathicsscript
+PACKAGE=mathics3
 
 # FIXME put some of the below in a common routine
 function finish {
@@ -16,7 +16,7 @@ fi
 
 
 cd ..
-source $PACKAGE/version.py
+source mathics/version.py
 echo $__version__
 
 for pyversion in $PYVERSIONS; do
