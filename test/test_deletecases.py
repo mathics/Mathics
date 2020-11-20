7568348ddce0a94a627bec2b5f54bcce573e7857
mmatera <matera@fisica.unlp.edu.ar>
2020-11-20 20:57:40 -0300

    adding tests for DeleteCases

---
+++ test/test_deletecases.py
@@ -0,0 +1,53 @@
+
+from mathics.session import MathicsSession
+
+from mathics.core.parser import parse, SingleLineFeeder
+from mathics.core.definitions import Definitions
+from mathics.core.evaluation import Evaluation
+import pytest
+
+
+session = MathicsSession(add_builtin=True, catch_interrupt=False)
+
+
+
+
+@pytest.mark.parametrize(
+    "str_expr,str_expected",
+    [
+        # DeleteCases tests
+        (r"DeleteCases[A,{_,_}]", "{{{1, 2, 3}, {a, b}, {1, 2, 3}}}"),
+        (r"DeleteCases[A,{_,_},1]", "{{{1, 2, 3}, {a, b}, {1, 2, 3}}}"),
+        (r"DeleteCases[A,{_,_},2]", "{{{1, 2, 3}, {a, b}, {1, 2, 3}}}"),
+        (r"DeleteCases[A,{_,_},3]", "{{{1, 2, 3}, {1, 2, 3}}}"),
+        (r"DeleteCases[A,{_,_},{3}]", "{{{1, 2, 3}, {1, 2, 3}},{a, b}, {10, 11}}"),
+        (r"DeleteCases[A,{_,_},{2,3}]", "{{{1, 2, 3}, {1, 2, 3}},{a, b}, {10, 11}}"),
+        (r"DeleteCases[A,{_,_},{1,3},2]", "{{{1, 2, 3}, {1, 2, 3}},{a, b}}"),
+    ],
+)
+
+def test_evaluation(str_expr: str, str_expected: str, message=""):
+    global session
+    session.evaluate('A={{{1, 2, 3}, {a, b}, {1, 2, 3}}, {a, b}, {10, 11}};')
+    result = session.evaluate(str_expr)
+    expected = session.evaluate(str_expected)
+
+    if message:
+        assert result == expected, message
+    else:
+        assert result == expected
+
+
+def test_exit():
+    global session
+    try:
+        session.evaluate("Exit[-37]")
+    except SystemExit as e:
+        assert e.code == -37
+
+
+def test_quit():
+    try:
+        session.evaluate("Quit[-37]")
+    except SystemExit as e:
+        assert e.code == -37
