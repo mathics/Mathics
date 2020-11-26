5bd9dddd3ac69553706db4cd14c909a7ae79ecd6
Pablo Emilio Escobar Gaviria <pablo-escobar@riseup.net>
2020-11-26 12:01:41 -0300

    Added the functions for compiling documentation to html back again

--- mathics/doc/doc.py
+++ mathics/doc/doc.py
@@ -407,6 +407,138 @@ def post_sub(text, post_substitutions):
     return text
 
 
+def escape_html(text, verbatim_mode=False, counters=None, single_line=False):
+    def repl_python(match):
+        return r"""<pre><![CDATA[
+%s
+]]></pre>""" % match.group(1).strip()
+    text, post_substitutions = pre_sub(PYTHON_RE, text, repl_python)
+
+    text = text.replace('&', '&amp;').replace('<', '&lt;').replace('>', '&gt;')
+
+    if not verbatim_mode:
+        def repl_quotation(match):
+            return r"&ldquo;%s&rdquo;" % match.group(1)
+        text = QUOTATIONS_RE.sub(repl_quotation, text)
+
+    if counters is None:
+        counters = {}
+
+    text = text.replace('"', '&quot;')
+    if not verbatim_mode:
+        def repl_latex(match):
+            return '%s<var>%s</var>%s' % (
+                match.group(1), match.group(2), match.group(3))
+
+        text = LATEX_RE.sub(repl_latex, text)
+
+        def repl_mathics(match):
+            text = match.group(1)
+            text = text.replace("\\'", "'")
+            text = text.replace(' ', '&nbsp;')
+            if text:
+                return "<code>%s</code>" % text
+            else:
+                return "'"
+
+        def repl_allowed(match):
+            content = _replace_all(match.group(1), [
+                ('&ldquo;', '"'), ('&rdquo;', '"'), ('&quot;', '"')])
+            return '<%s>' % content
+
+        text = MATHICS_RE.sub(repl_mathics, text)
+        for allowed in ALLOWED_TAGS:
+            text = ALLOWED_TAGS_RE[allowed].sub(repl_allowed, text)
+            text = text.replace('&lt;/%s&gt;' % allowed, '</%s>' % allowed)
+
+        def repl_dl(match):
+            text = match.group(1)
+            text = DL_ITEM_RE.sub(
+                lambda m: '<%(tag)s>%(content)s</%(tag)s>\n' % m.groupdict(),
+                text)
+            return '<dl>%s</dl>' % text
+
+        text = DL_RE.sub(repl_dl, text)
+
+        def repl_list(match):
+            tag = match.group('tag')
+            content = match.group('content')
+            content = LIST_ITEM_RE.sub(
+                lambda m: '<li>%s</li>' % m.group(1), content)
+            return '<%s>%s</%s>' % (tag, content, tag)
+
+        text = LIST_RE.sub(repl_list, text)
+
+        def repl_hypertext(match):
+            tag = match.group('tag')
+            content = match.group('content')
+            if tag == 'em':
+                return r'<em>%s</em>' % content
+            elif tag == 'url':
+                return r'<a href="%s">%s</a>' % (content, content)
+
+        text = HYPERTEXT_RE.sub(repl_hypertext, text)
+
+        def repl_console(match):
+            tag = match.group('tag')
+            content = match.group('content')
+            tag = 'div' if tag == 'console' else 'span'
+            content = content.strip()
+            pre = post = ''
+
+            # gets replaced for <br /> later by DocText.html()
+            content = content.replace('\n', '<br>')
+
+            return r'<%s class="console">%s%s%s</%s>' % (
+                tag, pre, content, post, tag)
+
+        text = CONSOLE_RE.sub(repl_console, text)
+
+        def repl_img(match):
+            src = match.group('src')
+            title = match.group('title')
+            return (r'<a href="/media/doc/%(src)s.pdf">'
+                    r'<img src="/media/doc/%(src)s.png" title="%(title)s" />'
+                    r'</a>') % {'src': src, 'title': title}
+        text = IMG_RE.sub(repl_img, text)
+
+        def repl_imgpng(match):
+            src = match.group('src')
+            title = match.group('title')
+            return (r'<img src="/media/doc/%(src)s" title="%(title)s" />') % {'src': src, 'title': title}
+        text = IMG_PNG_RE.sub(repl_imgpng, text)
+
+        def repl_ref(match):
+            # TODO: this is not an optimal solution - maybe we need figure
+            # numbers in the XML doc as well?
+            return r'the following figure'
+        text = REF_RE.sub(repl_ref, text)
+
+        def repl_subsection(match):
+            return '\n<h2 label="%s">%s</h2>\n' % (match.group(1), match.group(1))
+        text = SUBSECTION_RE.sub(repl_subsection, text)
+        text = SUBSECTION_END_RE.sub('', text)
+
+        text = text.replace("\\'", "'")
+    else:
+        text = text.replace(' ', '&nbsp;')
+        text = '<code>%s</code>' % text
+    text = text.replace("'", '&#39;')
+    text = text.replace('---', '&mdash;')
+    for key, (xml, tex) in SPECIAL_COMMANDS.items():
+        text = text.replace('\\' + key, xml)
+
+    if not single_line:
+        text = linebreaks(text)
+        text = text.replace('<br />', '\n').replace('<br>', '<br />')
+
+    text = post_sub(text, post_substitutions)
+
+    text = text.replace('<p><pre>', '<pre>').replace('</pre></p>', '</pre>')
+
+    return text
+
+
 class Tests(object):
     def __init__(self, part, chapter, section, tests):
         self.part, self.chapter = part, chapter
@@ -435,6 +567,9 @@ class DocElement(object):
         prev = collection[index - 1] if index > 0 else None
         next = collection[index + 1] if index < len(collection) - 1 else None
         return prev, next
+
+    def get_title_html(self):
+        return mark_safe(escape_html(self.title, single_line=True))
 
 
 class Documentation(DocElement):
@@ -849,6 +984,17 @@ class DocSection(DocElement):
     def get_collection(self):
         return self.chapter.sections
 
+    def html_data(self):
+        indices = set()
+        for test in self.doc.items:
+            indices.update(test.test_indices())
+        result = {}
+        for index in indices:
+            result[index] = xml_data.get((
+                self.chapter.part.title, self.chapter.title, self.title,
+                index))
+        return result
+
 
 class Doc(object):
     def __init__(self, doc):
@@ -910,6 +1056,11 @@ class Doc(object):
         return '\n'.join(item.latex(output) for item in self.items
                          if not item.is_private())
 
+    def html(self):
+        counters = {}
+        return mark_safe('\n'.join(item.html(counters) for item in self.items
+                         if not item.is_private()))
+
 
 class DocText(object):
     def __init__(self, text):
@@ -926,6 +1077,10 @@ class DocText(object):
 
     def latex(self, output):
         return escape_latex(self.text)
+
+    def html(self, counters=None):
+        result = escape_html(self.text, counters=counters)
+        return result
 
     def test_indices(self):
         return []
@@ -956,6 +1111,13 @@ class DocTests(object):
 
         return '\\begin{tests}%%\n%s%%\n\\end{tests}' % (
             '%\n'.join(testLatexStrings))
+
+    def html(self, counters=None):
+        if len(self.tests) == 0:
+            return "\n"
+        return '<ul class="tests">%s</ul>' % (
+            '\n'.join('<li>%s</li>' % test.html() for test in self.tests
+                      if not test.private))
 
     def test_indices(self):
         return [test.index for test in self.tests]
@@ -1047,3 +1209,11 @@ class DocTest(object):
                     'result']
         text += "\\end{testcase}"
         return text
+
+    def html(self):
+        result = '<div class="test"><span class="move"></span>'
+        result += '<ul class="test" id="test_%d">' % self.index
+        result += '<li class="test">%s</li>' % escape_html(self.test, True)
+        result += '</ul>'
+        result += '</div>'
+        return result
