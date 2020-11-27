ee0e1c0dad8db82c443e78bf782e64aa39b5bcbf
rocky <rb@dustyfeet.com>
2020-11-27 15:00:35 -0500

    Add System`VersionNumber
    Currently set to the most recent Mathematica release so that
    packages that test on $VersionNumber are not thwarted.

--- mathics/builtin/system.py
+++ mathics/builtin/system.py
@@ -11,7 +11,14 @@ import platform
 import sys
 import re
 
-from mathics.core.expression import Expression, Integer, String, Symbol, strip_context
+from mathics.core.expression import (
+    Expression,
+    Integer,
+    Real,
+    String,
+    Symbol,
+    strip_context,
+)
 from mathics.builtin.base import Builtin, Predefined
 from mathics import version_string
 from mathics.builtin.strings import StringExpression, to_regex
@@ -167,8 +174,8 @@ class MachineName(Predefined):
 class Names(Builtin):
     """
     <dl>
-    <dt>'Names["$pattern$"]'
-        <dd>returns the list of names matching $pattern$.
+      <dt>'Names["$pattern$"]'
+      <dd>returns the list of names matching $pattern$.
     </dl>
 
     >> Names["List"]
@@ -389,3 +396,22 @@ class Version(Predefined):
 
     def evaluate(self, evaluation) -> String:
         return String(version_string.replace("\n", " "))
+
+
+class VersionNumber(Predefined):
+    r"""
+    <dl>
+      <dt>'$VersionNumber'
+      <dd>is a real number which gives the current Wolfram Language kernel vesion number \Mathics tries to be compatible with.
+    </dl>
+
+    >> $VersionNumber
+    = ...
+    """
+
+    name = "$VersionNumber"
+
+    def evaluate(self, evaluation) -> String:
+        # Make this be whatever the latest Mathematica release is,
+        # assuming we are trying to be compatible with this.
+        return Real(12.1)
