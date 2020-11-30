d43346bbe2418dfcf4f27d4fb7cd1da80847f555
rocky <rb@dustyfeet.com>
2020-11-29 21:33:37 -0500

    Bump version to reflect we are in dev

--- mathics/version.py
+++ mathics/version.py
@@ -5,4 +5,4 @@
 # This file is suitable for sourcing inside POSIX shell as
 # well as importing into Python. That's why there is no
 # space around "=" below.
-__version__="1.1.0"  # noqa
+__version__="1.1.1dev"  # noqa
