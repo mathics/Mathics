4445408e0128578516cc8173b05ffe98c50e696e
rocky <rb@dustyfeet.com>
2020-12-15 15:55:40 -0500

    More tests

--- test/test_combinatorica.py
+++ test/test_combinatorica.py
@@ -7,41 +7,32 @@ from mathics.core.definitions import Definitions
 from mathics.core.evaluation import Evaluation
 import pytest
 
-
-def test_combinatorica():
-    session.evaluate(
-        """
-     Needs["DiscreteMath`CombinatoricaV0.9`"]
+session.evaluate(
+    """
+Needs["DiscreteMath`CombinatoricaV0.9`"]
      """
-    )
+)
 
-    # A number of examples from Implementing Discrete Mathematics by
-    # Steven Skiena and
-    # A number of examples from Computation Discrete Mathematics by
-    # Sriram Pemmaraju and Steven Skiena.
+# A number of examples from Implementing Discrete Mathematics by
+# Steven Skiena and
+# A number of examples from Computation Discrete Mathematics by
+# Sriram Pemmaraju and Steven Skiena.
 
-    # Page numbers below come from the first book
+# Page numbers below come from the first book
 
 
-    # Permutation[3] doesn't work
-    permutations3 = (
-        r"{{1, 2, 3}, {1, 3, 2}, {2, 1, 3}, {2, 3, 1}, {3, 1, 2}, {3, 2, 1}}"
-    )
+def test_combinatorica_permutations_1_1():
+
     for str_expr, str_expected, message in (
         (
             "Permute[{a, b, c, d}, Range[4]]",
             "{a, b, c, d}",
-            "Permute list with simple list",
+            "Permute list with simple list; 1.1 Page 3",
         ),
         (
             "Permute[{a, b, c, d}, {1,2,2,4}]",
             "Permute[{a, b, c, d}, {1,2,2,4}]",
-            "Incorrect permute: index 2 duplicated",
-        ),
-        (
-            "Permute[{A, B, C, D}, %s]" % permutations3,
-            "{{A, B, C}, {A, C, B}, {B, A, C}, {B, C, A}, {C, A, B}, {C, B, A}}",
-            "Permute",
+            "Incorrect permute: index 2 duplicated; 1.1 Page 3",
         ),
         (
             "LexicographicPermutations[{a,b,c,d}]",
@@ -53,74 +44,143 @@ def test_combinatorica():
             "{c, b, d, a}, {c, d, a, b}, {c, d, b, a}, "
             "{d, a, b, c}, {d, a, c, b}, {d, b, a, c}, "
             "{d, b, c, a}, {d, c, a, b}, {d, c, b, a}}",
-            "LexicographicPermuations, Page 4"
+            "LexicographicPermuations, 1.1.1 Page 4",
         ),
-
-        ("Map[RankPermutation, Permutations[Range[4]]]",
-         "Range[0, 23]",
-         "Permutations uses lexographic order"
-         ),
-
-        ("RandomPermutation1[20] === RandomPermutation2[20]",
-         "False",
-         "Not likey two of 20! permutations will be the same (different routines), Page 7"
-         ),
-        ("RandomPermutation1[20] === RandomPermutation1[20]",
-         "False",
-         "Not likley two of 20! permutations will be the same (same routine)"
-         ),
-        ("RankPermutation[{8, 9, 7, 1, 6, 4, 5, 3, 2}]", "321953", "RankPermutation"),
+        # NthPermutation does not work
         (
-            "Permute[{5,2,4,3,1}, InversePermutation[{5,2,4,3,1}]]",
-            "{1, 2, 3, 4, 5}",
-            "InversePermute",
+            "Map[RankPermutation, Permutations[Range[4]]]",
+            "Range[0, 23]",
+            "Permutations uses lexographic order; 1.1.2, Page 6",
+        ),
+        (
+            "RandomPermutation1[20] === RandomPermutation2[20]",
+            "False",
+            "Not likey two of the 20! permutations will be the same, 1.1.3, Page 7",
+        ),
+        (
+            "RandomPermutation1[20] === RandomPermutation1[20]",
+            "False",
+            "Not likley two of 20! permutations will be the same (same routine)",
         ),
         (
             "MinimumChangePermutations[{a,b,c}]",
             "{{a, b, c}, {b, a, c}, {c, a, b}, {a, c, b}, {b, c, a}, {c, b, a}}",
-            "MinimumChangePermuations, Page 11",
+            "MinimumChangePermuations; 1.1.4, Page 11",
         ),
         (
             "Union[Permutations[{a,a,a,a,a}]]",
             "{{a, a, a, a, a}}",
-            "simple but wasteful Permutation duplication elimination, Page 12"
+            "simple but wasteful Permutation duplication elimination, 1.1.5, Page 12",
         ),
         (
             "DistinctPermutations[{1,1,2,2}]",
             "{{1, 1, 2, 2}, {1, 2, 1, 2}, {1, 2, 2, 1}, "
             "{2, 1, 1, 2}, {2, 1, 2, 1}, {2, 2, 1, 1}}",
-            "DisctinctPermutations of multiset Binomial[6,3] permutations, Page 14"
+            "DisctinctPermutations of multiset Binomial[6,3] permutations, 1.1.5, Page 14",
         ),
-        (
-            "Multinomial[3,3]",
-            "20",
-            "The built-in function Multinomial, Page 14"
-        ),
+        ("Multinomial[3,3]", "20", "The built-in function Multinomial, Page 14"),
         (
             "DistinctPermutations[{A,B,C}]",
             "{{A, B, C}, {A, C, B}, {B, A, C}, {B, C, A}, {C, A, B}, {C, B, A}}",
-            "DisctinctPermutations all n! permutations, Page 14"
+            "DisctinctPermutations all n! permutations, Page 14",
+        ),
+        (
+            "BinarySearch[Table[2i,{i, 30}],40]",
+            "20",
+            "BinarySearch: 40 is one of the first 30 even numbers; 1.1.6, Page 16",
+        ),
+        (
+            "BinarySearch[Table[2i,{i, 30}],41]",
+            "41/2",
+            "BinarySearch: BinarySearch: 41 is not even; 1.1.6, Page 16",
         ),
         (
             "Sort[ Subsets [Range[4]],(Apply[Plus, #1]<=Apply[Plus,#2])& ]",
             "{{}, {1}, {2}, {3}, {1, 2}, {4}, {1, 3}, {1, 4}, {2, 3}, {2, 4}, "
             "{1, 2, 3}, {3, 4}, {1, 2, 4}, {1, 3, 4}, {2, 3, 4}, {1, 2, 3, 4}}",
-            "Sort to total order subsets, Page 15"
+            "Sort to total order subsets, Page 15",
+        ),
+        (
+            "InversePermutation[{4,8,5,2,1,3,7,6}]",
+            "{5, 4, 6, 1, 3, 8, 7, 2}",
+            "InversePermutation: 7 is fixed point. Page 18",
+        ),
+        (
+            "star = Automorphisms[Star[5]]",
+            "{{1, 2, 3, 4, 5}, {1, 2, 4, 3, 5}, {1, 3, 2, 4, 5}, {1, 3, 4, 2, 5}, "
+            "{1, 4, 2, 3, 5}, {1, 4, 3, 2, 5}, {2, 1, 3, 4, 5}, {2, 1, 4, 3, 5}, "
+            "{2, 3, 1, 4, 5}, {2, 3, 4, 1, 5}, {2, 4, 1, 3, 5}, {2, 4, 3, 1, 5}, "
+            "{3, 1, 2, 4, 5}, {3, 1, 4, 2, 5}, {3, 2, 1, 4, 5}, {3, 2, 4, 1, 5}, "
+            "{3, 4, 1, 2, 5}, {3, 4, 2, 1, 5}, {4, 1, 2, 3, 5}, {4, 1, 3, 2, 5}, "
+            "{4, 2, 1, 3, 5}, {4, 2, 3, 1, 5}, {4, 3, 1, 2, 5}, {4, 3, 2, 1, 5}}",
+            "Automorphisms, Page 19",
+        ),
+    ):
+        check_evaluation(str_expr, str_expected, message)
+
+
+def test_combinatorica_permutations_1_2():
+
+    for str_expr, str_expected, message in (
+        (
+            "MultiplicationTable[Permutations[Range[3]], Permute ]",
+            "{{1, 2, 3, 4, 5, 6}, "
+            "{2, 1, 5, 6, 3, 4}, "
+            "{3, 4, 1, 2, 6, 5}, "
+            "{4, 3, 6, 5, 1, 2}, "
+            "{5, 6, 2, 1, 4, 3}, "
+            "{6, 5, 4, 3, 2, 1}}",
+            "Symmetric group S_n. S_n is not commutative. 1.2 Page 17"
+        ),
+        (
+            "InversePermutation[{4,8,5,2,1,3,7,6}]",
+            "{5, 4, 6, 1, 3, 8, 7, 2}",
+            "InversePermutation: 7 is fixed point. 1.2 Page 18",
+        ),
+        (
+            "star = Automorphisms[Star[5]]",
+            "{{1, 2, 3, 4, 5}, {1, 2, 4, 3, 5}, {1, 3, 2, 4, 5}, {1, 3, 4, 2, 5}, "
+            "{1, 4, 2, 3, 5}, {1, 4, 3, 2, 5}, {2, 1, 3, 4, 5}, {2, 1, 4, 3, 5}, "
+            "{2, 3, 1, 4, 5}, {2, 3, 4, 1, 5}, {2, 4, 1, 3, 5}, {2, 4, 3, 1, 5}, "
+            "{3, 1, 2, 4, 5}, {3, 1, 4, 2, 5}, {3, 2, 1, 4, 5}, {3, 2, 4, 1, 5}, "
+            "{3, 4, 1, 2, 5}, {3, 4, 2, 1, 5}, {4, 1, 2, 3, 5}, {4, 1, 3, 2, 5}, "
+            "{4, 2, 1, 3, 5}, {4, 2, 3, 1, 5}, {4, 3, 1, 2, 5}, {4, 3, 2, 1, 5}}",
+            "Automorphisms, 1.2.3 Page 19",
+        ),
+        (
+            "relation = SamenessRelation[star]",
+            "{{1, 1, 1, 1, 0}, "
+            "{1, 1, 1, 1, 0}, "
+            "{1, 1, 1, 1, 0}, "
+            "{1, 1, 1, 1, 0}, "
+            "{0, 0, 0, 0, 1}}",
+            "Sameness, 1.2.3 Page 19",
+        ),
+        # (
+        #     "PermutationGroupQ[Range[4], {4, 2, 3, 1}]",
+        #     "True",
+        #     "PermutationGroupQ, 1.2.3 Page 20",
+        # ),
+    ):
+        check_evaluation(str_expr, str_expected, message)
+
+
+def test_combinatorica_rest():
+
+    # Permutation[3] doesn't work
+    permutations3 = (
+        r"{{1, 2, 3}, {1, 3, 2}, {2, 1, 3}, {2, 3, 1}, {3, 1, 2}, {3, 2, 1}}"
+    )
+    for str_expr, str_expected, message in (
+        (
+            "Permute[{A, B, C, D}, %s]" % permutations3,
+            "{{A, B, C}, {A, C, B}, {B, A, C}, {B, C, A}, {C, A, B}, {C, B, A}}",
+            "Permute",
         ),
         (
             "Subsets[Range[3]]",
             "{{}, {1}, {2}, {3}, {1, 2}, {1, 3}, {2, 3}, {1, 2, 3}}",
             "Subsets",
-        ),
-        (
-            "BinarySearch[Table[2i,{i, 30}],40]",
-            "20",
-            "BinarySearch: 40 is one of the first 30 even numbers, Page 16"
-        ),
-        (
-            "BinarySearch[Table[2i,{i, 30}],41]",
-            "41/2",
-            "BinarySearch: BinarySearch: 41 is not even"
         ),
         (
             "BinarySearch[{2, 3, 9}, 7] // N",
@@ -153,44 +213,10 @@ def test_combinatorica():
             "2",
             "BinarySearch - find where key is a list",
         ),
-        # (
-        #     "TableForm[ MultiplicationTable[Permutations[Range[3]], Permute ] ]",
-        #     "1   2   3   4   5   6\n"
-        #     "2   1   5   6   3   4\n"
-        #     "3   4   1   2   6   5\n"
-        #     "4   3   6   5   1   2\n"
-        #     "5   6   2   1   4   3\n"
-        #     "6   5   4   3   2   1\n",
-        #     "Symmetric group S_n. S_n is not commutative. Page 17"
-        # ),
         (
             "InversePermutation[{4,8,5,2,1,3,7,6}]",
             "{5, 4, 6, 1, 3, 8, 7, 2}",
             "InversePermutation: 7 is fixed point. Page 18",
-        ),
-        (
-            "star = Automorphisms[Star[5]]",
-            "{{1, 2, 3, 4, 5}, {1, 2, 4, 3, 5}, {1, 3, 2, 4, 5}, {1, 3, 4, 2, 5}, "
-            "{1, 4, 2, 3, 5}, {1, 4, 3, 2, 5}, {2, 1, 3, 4, 5}, {2, 1, 4, 3, 5}, "
-            "{2, 3, 1, 4, 5}, {2, 3, 4, 1, 5}, {2, 4, 1, 3, 5}, {2, 4, 3, 1, 5}, "
-            "{3, 1, 2, 4, 5}, {3, 1, 4, 2, 5}, {3, 2, 1, 4, 5}, {3, 2, 4, 1, 5}, "
-            "{3, 4, 1, 2, 5}, {3, 4, 2, 1, 5}, {4, 1, 2, 3, 5}, {4, 1, 3, 2, 5}, "
-            "{4, 2, 1, 3, 5}, {4, 2, 3, 1, 5}, {4, 3, 1, 2, 5}, {4, 3, 2, 1, 5}}",
-            "Automorphisms, Page 19"
-        ),
-        (
-            "KSubsets[Range[5], 3]",
-            "{{1, 2, 3}, {1, 2, 4}, {1, 2, 5}, {1, 3, 4}, {1, 3, 5}, {1, 4, 5}, "
-            "{2, 3, 4}, {2, 3, 5}, {2, 4, 5}, {3, 4, 5}}",
-            "Ksubsets",
-        ),
-        (
-            "Compositions[5,3]",
-            "{{0, 0, 5}, {0, 1, 4}, {0, 2, 3}, {0, 3, 2}, {0, 4, 1}, {0, 5, 0}, "
-            "{1, 0, 4}, {1, 1, 3}, {1, 2, 2}, {1, 3, 1}, {1, 4, 0}, {2, 0, 3}, "
-            "{2, 1, 2}, {2, 2, 1}, {2, 3, 0}, {3, 0, 2}, {3, 1, 1}, {3, 2, 0}, "
-            "{4, 0, 1}, {4, 1, 0}, {5, 0, 0}}",
-            "Compositions",
         ),
         # (
         #     "SetPartitions[3]",
@@ -200,7 +226,7 @@ def test_combinatorica():
         (
             "TransposePartition[{8, 6, 4, 4, 3, 1}]",
             "{6, 5, 5, 4, 2, 2, 1, 1}",
-            "TransposePartition"
+            "TransposePartition",
         ),
     ):
         check_evaluation(str_expr, str_expected, message)
