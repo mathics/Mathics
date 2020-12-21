cb17bb458436bb6d25e85f98c85597b10ca7942b
rocky <rb@dustyfeet.com>
2020-12-20 23:23:00 -0500

    In prepration for 1.1.1 release

--- mathics/packages/DiscreteMath/Kernel/init.m
+++ mathics/packages/DiscreteMath/Kernel/init.m
@@ -9,11 +9,11 @@ by Steven S. Skiena, Addison-Wesley Publishing Co., Advanced Book Program,
 350 Bridge Parkway, Redwood City CA 94065.  ISBN 0-201-50943-1.
 For ordering information, call 1-800-447-2226.
  *)
-Get[ "DiscreteMath`CombinatoricaV0.6"]
-Get[ "DiscreteMath`CombinatoricaV0.9"]
+Get[ "DiscreteMath`CombinatoricaV0.6" ]
+Get[ "DiscreteMath`CombinatoricaV0.9" ]
 
 (* The below is from https://library.wolfram.com/infocenter/MathSource/6825/
  An alpha test version is at:
  http://www.nic.funet.fi/index/Science/math/mathematica/Logic/RSolve.m
  *)
-Get[ "DiscreteMath`RSolve`"]
+Get[ "DiscreteMath`RSolve`" ]
