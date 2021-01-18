bf750067edcb3fba1e9c959794ddae3fed2c7490
rocky <rb@dustyfeet.com>
2021-01-18 17:18:45 -0500

    More import conversions

--- test/test_parser/test_util.py
+++ test/test_parser/test_util.py
@@ -1,8 +1,14 @@
 import unittest
 
+from mathics_scanner import (
+    IncompleteSyntaxError,
+    InvalidSyntaxError,
+    MultiLineFeeder,
+    SingleLineFeeder,
+)
+
 from mathics.core.definitions import Definitions
-from mathics.core.parser import parse, InvalidSyntaxError, IncompleteSyntaxError
-from mathics.core.parser.feed import SingleLineFeeder, MultiLineFeeder
+from mathics.core.parser import parse
 
 
 definitions = Definitions(add_builtin=True)
@@ -41,13 +47,13 @@ class SingleLineParserTests(UtilTests):
         self.assertTrue(expr1.same(expr2))
 
     def test_continuation(self):
-        self.incomplete_error('Sin[')
-        self.check('Sin[\n0]', 'Sin[0]')
-        self.check('Sin[\n\n0]', 'Sin[0]')
+        self.incomplete_error("Sin[")
+        self.check("Sin[\n0]", "Sin[0]")
+        self.check("Sin[\n\n0]", "Sin[0]")
 
     def test_trailing_backslash(self):
-        self.incomplete_error('x \\')
-        self.check('x \\\ny', 'Times[x, y]')
+        self.incomplete_error("x \\")
+        self.check("x \\\ny", "Times[x, y]")
 
 
 class MultiLineParserTests(UtilTests):
@@ -58,28 +64,30 @@ class MultiLineParserTests(UtilTests):
         self.assertTrue(expr1.same(expr2))
 
     def test_trailing_backslash(self):
-        self.incomplete_error('x \\')
-        self.check('x \\\ny', 'Times[x, y]')
+        self.incomplete_error("x \\")
+        self.check("x \\\ny", "Times[x, y]")
 
     def test_continuation(self):
-        self.incomplete_error('Sin[')
-        self.check('Sin[\n0]', 'Sin[0]')
-        self.check('Sin[0\n]', 'Sin[0]')
-        self.check('Sin[\n\n0]', 'Sin[0]')
+        self.incomplete_error("Sin[")
+        self.check("Sin[\n0]", "Sin[0]")
+        self.check("Sin[0\n]", "Sin[0]")
+        self.check("Sin[\n\n0]", "Sin[0]")
 
     def test_CompoundExpression(self):
-        self.check('f[a;\nb]', 'f[CompoundExpression[a, b]]')
-        self.check('f[a;\nb;\nc;]', 'f[CompoundExpression[a, b, c, Null]]')
-        self.check('f[a;\nb;\nc;\n]', 'f[CompoundExpression[a, b, c, Null]]')
+        self.check("f[a;\nb]", "f[CompoundExpression[a, b]]")
+        self.check("f[a;\nb;\nc;]", "f[CompoundExpression[a, b, c, Null]]")
+        self.check("f[a;\nb;\nc;\n]", "f[CompoundExpression[a, b, c, Null]]")
 
-        self.check('a;^b', 'Power[CompoundExpression[a, Null], b]')
+        self.check("a;^b", "Power[CompoundExpression[a, Null], b]")
 
-        feeder = MultiLineFeeder('a;\n^b')
-        self.compare(parse(definitions, feeder), self.parse('CompoundExpression[a, Null]'))
+        feeder = MultiLineFeeder("a;\n^b")
+        self.compare(
+            parse(definitions, feeder), self.parse("CompoundExpression[a, Null]")
+        )
         self.assertRaises(InvalidSyntaxError, lambda f: parse(definitions, f), feeder)
 
     def test_Span(self):
-        self.check('a;;^b', 'Power[Span[a, All], b]')
-        feeder = MultiLineFeeder('a;;\n^b')
-        self.compare(parse(definitions, feeder), self.parse('Span[a, All]'))
+        self.check("a;;^b", "Power[Span[a, All], b]")
+        feeder = MultiLineFeeder("a;;\n^b")
+        self.compare(parse(definitions, feeder), self.parse("Span[a, All]"))
         self.assertRaises(InvalidSyntaxError, lambda f: parse(definitions, f), feeder)
