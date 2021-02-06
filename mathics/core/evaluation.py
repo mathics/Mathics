c41566de5269e972991d9cac34310d0ce0766e43
rocky <rb@dustyfeet.com>
2021-02-06 12:28:49 -0500

    Adjust for external Mathics Scanner
    Customize LineFeeder Routines for Mathics.

--- mathics/core/evaluation.py
+++ mathics/core/evaluation.py
@@ -59,7 +59,6 @@ class WLThrowInterrupt(EvaluationInterrupt):
         self.value = value
 
 
-
 def _thread_target(request, queue) -> None:
     try:
         result = request()
@@ -229,11 +228,7 @@ class Output(object):
 
 class Evaluation(object):
     def __init__(
-            self,
-            definitions=None,
-            output=None,
-            format="text",
-            catch_interrupt=True
+        self, definitions=None, output=None, format="text", catch_interrupt=True
     ) -> None:
         from mathics.core.definitions import Definitions
         from mathics.core.expression import Symbol
@@ -263,9 +258,9 @@ class Evaluation(object):
 
     def parse(self, query):
         "Parse a single expression and print the messages."
-        from mathics.core.parser import SingleLineFeeder
+        from mathics.core.parser import MathicsSingleLineFeeder
 
-        return self.parse_feeder(SingleLineFeeder(query))
+        return self.parse_feeder(MathicsSingleLineFeeder(query))
 
     def parse_evaluate(self, query, timeout=None):
         expr = self.parse(query)
