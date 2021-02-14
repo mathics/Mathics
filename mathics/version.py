25ab137558c8649a43538511c1577b19fd6d1a99
rocky <rb@dustyfeet.com>
2021-02-14 11:32:22 -0500

    Get ready for release 2.0.0

--- mathics/version.py
+++ mathics/version.py
@@ -4,4 +4,4 @@
 # This file is suitable for sourcing inside POSIX shell as
 # well as importing into Python. That's why there is no
 # space around "=" below.
-__version__="2.0.0.rc1"  # noqa
+__version__="2.0.0"  # noqa
