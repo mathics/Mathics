57a3296e277cf51ec18dc9e6c33e7f46eb680aaf
rocky <rb@dustyfeet.com>
2021-03-09 08:06:14 -0500

    CI for windows has differenet codepage ...
    and doesn't have mu character

---
+++ test/test_inout.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from .helper import check_evaluation
+import os
+import sys
+
+if sys.platform not in {"win32",} and not os.environ.get("CI"):
+    def test_non_win32_print():
+        for str_expr, str_expected, message in (
+            (
+             'Print["\\[Mu]"]',
+             "μ"
+            ),
+        ):
+            check_evaluation(str_expr, str_expected, message)
