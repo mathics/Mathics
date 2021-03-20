77b93ad4070287249dd1602b043f1553ad9f9160
rocky <rb@dustyfeet.com>
2021-03-20 14:25:36 -0400

    Small Cython tweaks

--- mathics/builtin/base.py
+++ mathics/builtin/base.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+# cython: language_level=3
 
 import re
 import sympy
--- mathics/builtin/files.py
+++ mathics/builtin/files.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+# cython: language_level=3
 
 """
 File Operations
@@ -2186,7 +2187,8 @@ class Get(PrefixOperator):
             result = {}
             trace_get = evaluation.parse('Settings`$TraceGet')
             if options["System`Trace"].to_python() or trace_get.evaluate(evaluation) == SymbolTrue:
-                result["TraceFn"] = print
+                import builtins
+                result["TraceFn"] = builtins.print
             else:
                 result["TraceFn"] = None
 
