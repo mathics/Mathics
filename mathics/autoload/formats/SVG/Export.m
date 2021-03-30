66cea3c0e1ae5e89f7960e7003cd7ea408813617
mmatera <matera@fisica.unlp.edu.ar>
2021-03-30 16:09:47 -0300

    fix Export to svg for general expressions

--- mathics/autoload/formats/SVG/Export.m
+++ mathics/autoload/formats/SVG/Export.m
@@ -4,10 +4,12 @@ Begin["System`Convert`TextDump`"]
 
 
 SVGExport[filename_, expr_, opts___] := 
-  Module[{strm, data, p}, 
+  Module[{strm, data, p, expr2}, 
     strm = OpenWrite[filename];
     If[strm === $Failed, Return[$Failed]];
-    data=ToString[MathMLForm[expr]];
+    expr2 = If[Head[expr]=!=System`Graphics, System`Graphics[{System`Inset[expr]}], expr];
+	 expr2= MathMLForm[expr2];
+    data=ToString[expr2];
     p = StringPosition[data, "data:image/svg+xml;base64"][[1]][[2]];
     data = StringTake[data  ,{p+2,-19}];
     WriteString[strm, System`Convert`B64Dump`B64Decode[data]];
