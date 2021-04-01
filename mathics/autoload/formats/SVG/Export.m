e2c0558283424cf1f5a605ba6fb583f101237707
mmatera <matera@fisica.unlp.edu.ar>
2021-04-01 10:55:12 -0300

    less kriptic code in xport

--- mathics/autoload/formats/SVG/Export.m
+++ mathics/autoload/formats/SVG/Export.m
@@ -4,14 +4,16 @@ Begin["System`Convert`TextDump`"]
 
 
 SVGExport[filename_, expr_, opts___] := 
-  Module[{strm, data, p, expr2}, 
+  Module[{strm, data, p, q, expr2}, 
     strm = OpenWrite[filename];
     If[strm === $Failed, Return[$Failed]];
     expr2 = If[Head[expr]=!=System`Graphics, System`Graphics[{System`Inset[ToString[expr]]}], expr];
 	 expr2= MathMLForm[expr2];
     data=ToString[expr2];
     p = StringPosition[data, "data:image/svg+xml;base64"][[1]][[2]];
-    data = StringTake[data  ,{p+2,-19}];
+    (*Let's assume that the end of the string is reached just before the last quote. *)
+    q = StringPosition[data,"\""][[-1]][[-2]];
+    data = StringTake[data  ,{p+2,q-1}];
     WriteString[strm, System`Convert`B64Dump`B64Decode[data]];
     Close[strm];
   ]
