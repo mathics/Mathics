762a4692a4790b57d5da68cfccd7dd0d8c48da38
mmatera <matera@fisica.unlp.edu.ar>
2021-04-22 09:44:03 -0300

    a different approach: control the processing of OneIdentity from evaluation

---
+++ test/test_assignment.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from .helper import check_evaluation
+import pytest
+
+from mathics_scanner.errors import IncompleteSyntaxError
+
+
+
+str_test_set_with_oneidentity = """
+SetAttributes[SUNIndex, {OneIdentity}];
+SetAttributes[SUNFIndex, {OneIdentity}];
+
+SUNIndex[SUNFIndex[___]]:=
+	(Print["This error shouldn't be triggered here!"];
+	Abort[]);
+
+SUNFIndex[SUNIndex[___]]:=
+	(Print["This error shouldn't be triggered here!"];
+	Abort[]);
+
+SUNIndex /: MakeBoxes[SUNIndex[p_], TraditionalForm]:=ToBoxes[p, TraditionalForm];
+
+SUNFIndex /: MakeBoxes[SUNFIndex[p_], TraditionalForm]:=ToBoxes[p, TraditionalForm];
+"""
+
+
+def test_setdelayed_oneidentity():
+    expr = ""
+    for line in str_test_set_with_oneidentity.split("\n"):
+        if line in ("", "\n"):
+            continue
+        expr = expr + line
+        try:
+            check_evaluation(expr, "Null", to_string_expr=False, to_string_expected=False)
+            expr = ""
+        except IncompleteSyntaxError:
+            continue
