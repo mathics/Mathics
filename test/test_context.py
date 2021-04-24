718a0250a58013ec4080681fe888cbac3634575b
mmatera <matera@fisica.unlp.edu.ar>
2021-04-24 16:52:48 -0300

    fix some tests

--- test/test_context.py
+++ test/test_context.py
@@ -42,7 +42,9 @@ End[];
 
 EndPackage[];
 """
-
+@pytest.mark.skip(
+        reason="The fix for this is implemented in a different PR..."
+    )
 def test_context1():
     expr = ""
     for line in str_test_context.split("\n"):
