a4f38c3ace48459c37139c0041e622ad1a2b2ed6
rocky <rb@dustyfeet.com>
2021-06-24 07:09:41 -0400

    Fill out descriptions on section names

--- mathics/builtin/combinatorial.py
+++ mathics/builtin/combinatorial.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 """
-Combinatorial Functions
+Combinatorics
+
+Combinatorics is an area of mathematics primarily concerned with counting, both as a means and an end in obtaining results, and certain properties of finite structures.
+
+It is closely related to many other areas of mathematics and has many applications ranging from logic to statistical physics, from evolutionary biology to computer science, etc.
 """
 
 
--- mathics/builtin/control.py
+++ mathics/builtin/control.py
@@ -2,7 +2,13 @@
 # -*- coding: utf-8 -*-
 
 """
-Control Statements
+Procedural Programming
+
+Procedural programming is a programming paradigm, derived from imperative programming, based on the concept of the procedure call.
+
+Procedures (a type of routine or subroutine) simply contain a series of computational steps to be carried out. Any given procedure might be called at any point during a program's execution, including by other procedures or itself.
+
+Procedural functions and are integrated into Mathics symbolic programming environment.
 """
 
 
