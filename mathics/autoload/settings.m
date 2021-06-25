e34443aae588f217a96a5cedc829a822faf2be5e
rocky <rb@dustyfeet.com>
2021-06-25 16:17:07 -0400

    Add period to the ends of usage string sentences.
    The English, she is hard on me.

--- mathics/autoload/settings.m
+++ mathics/autoload/settings.m
@@ -2,11 +2,11 @@
 (* Settings for Mathics Core *)
 (*****************************)
 
-Settings`$TraceGet::usage = "If this Boolean variable is set True, 'Get' traces the lines it reads that start a new expression";
+Settings`$TraceGet::usage = "If this Boolean variable is set True, 'Get' traces the lines it reads that start a new expression.";
 Settings`$TraceGet = False
 Unprotect[Settings`$TraceGet]
 
 
-Settings`$PreferredBackendMethod::usage = "Set this do whether to use mpmath, numpy or Sympy for numeric and symbolic constants and methods when there is a choice";
+Settings`$PreferredBackendMethod::usage = "This sets whether to use mpmath, numpy or Sympy for numeric and symbolic constants and methods, when there is a choice.";
 Settings`$PreferredBackendMethod = "sympy"
 Unprotect[Settings`$PreferredBackendMethod]
