0fea8b6d9a49f5ec6caff5abfee4bc6c54153a33
rocky <rb@dustyfeet.com>
2021-08-21 10:34:24 -0400

    Mostly cosmetic revision of numeric.py
    * Put builtins in alphabetic order
    * Add summary text for some built ins
    * No hard line breaks on doc text
    * Move non-showing test code in Rationalize out to a pytest

--- test/test_numeric.py
+++ test/test_numeric.py
@@ -2,6 +2,54 @@
 from .helper import check_evaluation
 
 
+def test_rationalize():
+    # Some of the Rationalize tests were taken from Symja's tests and docs
+    for str_expr, str_expected in (
+        (
+            "Rationalize[42]",
+            "42",
+        ),
+        # We get 3 / 1 instead of 3
+        # (
+        #     "Rationalize[3, 1]",
+        #     "3",
+        # ),
+        (
+            "Rationalize[N[Pi] + 0.8 I, 0]",
+            "245850922 / 78256779 + 4 I / 5",
+        ),
+        (
+            "Rationalize[1.6 + 0.8 I]",
+            "8 / 5 + 4 I / 5",
+        ),
+        (
+            "Rationalize[17 / 7]",
+            "17 / 7",
+        ),
+        (
+            "Rationalize[6.75]",
+            "27 / 4",
+        ),
+        (
+            "Rationalize[0.25+I*0.33333]",
+            "1 / 4 + I / 3",
+        ),
+        (
+            "Rationalize[N[Pi] + 0.8 I, 1*^-6]",
+            "355 / 113 + 4 I / 5",
+        ),
+        (
+            "Rationalize[x]",
+            "x",
+        ),
+        (
+            "Table[Rationalize[E, 0.1^n], {n, 1, 10}]",
+            "{8 / 3, 19 / 7, 87 / 32, 193 / 71, 1071 / 394, 2721 / 1001, 15062 / 5541, 23225 / 8544, 49171 / 18089, 419314 / 154257}",
+        ),
+    ):
+        check_evaluation(str_expr, str_expected)
+
+
 def test_realvalued():
     for str_expr, str_expected in (
         (
